9195899a89ec386c439022ca94d031fed33cf7d2
Jakob R <Sicari-s@users.noreply.github.com>
2022-02-23 22:53:41 +0100

    Edited 133

--- Medium/133. Clone Graph.py
+++ Medium/133. Clone Graph.py
@@ -1,53 +1,35 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
+# class Node:
+#     def __init__(self, val = 0, neighbors = None):
+#         self.val = val
+#         self.neighbors = neighbors if neighbors is not None else []
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-
-        def dfs(node: 'Node') -> None:
-            if not node:
-                return
-
-            if not (node.val in self.newNodes):
-                self.newNodes[node.val] = Node(node.val)
-                for n in node.neighbors:
-                    dfs(n)
-
-        def connectNeighbors(node: 'Node'):
-            if not (node.val in self.visited):
-                self.visited.add(node.val)
-                self.newNodes[node.val].neighbors = [self.newNodes[neighbor.val] for neighbor in node.neighbors]
-                for neighbor in node.neighbors:
-                    connectNeighbors(neighbor)
-
-        return self.cloneGraphStolen(node)
-
+        
+        def connect_neighbors(original_node: 'Node') -> None:
+            # mark as visited to not visit node twice
+            self.visited.add(original_node.val)
+            # create clone or get if already created
+            cloned = get_clone(original_node.val)
+            # set neighbors for clones
+            cloned.neighbors = [get_clone(neighbor.val) for neighbor in original_node.neighbors]
+            
+            # keep reconstructing cloned graph with neighbors
+            for neighbor in original_node.neighbors:
+                if not (neighbor.val in self.visited):
+                    connect_neighbors(neighbor)
+        
+        def get_clone(val):
+            # faster than dict.setdefault as Node() does not need to be constructed and passed every time
+            if not (val in self.cloned_nodes):
+                self.cloned_nodes[val] = Node(val)
+            return self.cloned_nodes[val]
+        
         if not node:
             return None
-
-        self.newNodes = dict()
+        
+        self.cloned_nodes = dict()
         self.visited = set()
-        dfs(node)
-        connectNeighbors(node)
-        return self.newNodes[1]
-
-    # mine was slow first two times, turns out not really
-    # heavyly changed and optimized
-
-    def cloneGraphStolen(self, node: 'Node') -> 'Node':
-        def dfs(node):
-            copy_node = Node(node.val)
-            newNodes[node.val] = copy_node
-
-            copy_node.neighbors = [newNodes[neighbor.val] if (neighbor.val in newNodes) else dfs(neighbor) for neighbor in node.neighbors]
-
-            return copy_node
-
-        newNodes = dict()
-        return dfs(node) if node else None
+        
+        connect_neighbors(node)
+        return self.cloned_nodes[1]
