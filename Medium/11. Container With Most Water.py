6b749f0ba879880ab4749109c3e1c54cdd29fca8
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-04-05 09:17:46 +0200

    [11.] Updated

--- Medium/11. Container With Most Water.py
+++ Medium/11. Container With Most Water.py
@@ -1,44 +1,30 @@
-def BrutemaxArea(self, height: [int]) -> int:
-    retVal = 0
-    for i in range(len(height) - 1):
-        for j in range(i + 1, len(height)):
-            if (j - i) * min(height[i], height[j]) > retVal:
-                retVal = (j - i) * min(height[i], height[j])
-            # print(i, j)
-            # print(min(height[i], height[j]))
-
-    return retVal
-
-
-def decentEstimationButDontWorkmaxArea(self, height: [int]) -> int:
-    retVal = 0
-    middle = (len(height) - 1) / 2
-    pillarValues = {}
-    BiggestTwo_i_score = [[0, 0], [0, 0]]
-    for i in range(len(height)):
-        curScore = height[i] * (i - middle)
-        pillarValues[i] = curScore
-        if curScore > BiggestTwo_i_score[0][1]:
-            BiggestTwo_i_score[0] = [i, curScore]
-        elif curScore < BiggestTwo_i_score[1][1]:
-            BiggestTwo_i_score[1] = [i, curScore]
-    retVal = (BiggestTwo_i_score[0][0] - BiggestTwo_i_score[1][0]) * min(
-        height[BiggestTwo_i_score[0][0]], height[BiggestTwo_i_score[1][0]])
-    print(BiggestTwo_i_score)
-    return retVal
+from typing import List
 
 
 class Solution:
-    def maxArea(self, height: [int]) -> int:
-        upperb = len(height) - 1
+    def maxArea(self, height: List[int]) -> int:
+        # use a sliding window, lower and upper bound, and store heights for efficiency
         lowerb = 0
-        curMax = 0
-        while upperb != lowerb:
-            curScore = (upperb - lowerb) * min(height[upperb], height[lowerb])
-            if (curScore > curMax):
-                curMax = curScore
-            if height[upperb] < height[lowerb]:
-                upperb -= 1
-            else:
+        lowerb_height = height[lowerb]
+
+        upperb = len(height) - 1
+        upperb_height = height[upperb]
+
+        res = 0
+        # while sliding window has volume
+        while lowerb < upperb:
+            # calculate volume of current sliding window
+            volume = (upperb - lowerb) * min(lowerb_height, upperb_height)
+            # if it is the largest one so far, set it as the result
+            if (volume > res):
+                res = volume
+
+            # greedily change the sliding window so that the next volume is maximized
+            if lowerb_height < upperb_height:
                 lowerb += 1
-        return curMax
+                lowerb_height = height[lowerb]
+            else:
+                upperb -= 1
+                upperb_height[upperb]
+
+        return res
