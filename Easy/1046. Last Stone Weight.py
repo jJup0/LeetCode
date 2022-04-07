51a20a7fa13f614c40e2a67480e6a7625d7e96fc
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-04-07 08:57:01 +0200

    [1046.] Added old solution from 2020

---
+++ Easy/1046. Last Stone Weight.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()
+            s3 = 0
+            biggestS3 = 0
+            for _ in range(round(len(stones) + 0.9/2)):
+                if len(stones) < 2 or biggestS3 > stones[-2]:
+                    break
+                if s3 >= stones[-2]:
+                    s1 = s3
+                    s2 = stones.pop(-1)
+                else:
+                    s1, s2 = stones.pop(-1), stones.pop(-1)
+                    if s3:
+                        stones.insert(0, s3)
+                s3 = abs(s1 - s2)
+                if s3 > biggestS3:
+                    biggestS3 = s3
+            if s3:
+                stones.append(s3)
+        if stones:
+            return stones[0]
+        return 0
