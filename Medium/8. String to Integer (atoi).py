82e53b2d0100b4333bf47f4f22f6dc1ec192d12c
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-04-28 11:50:02 +0200

    [8.] Updated

--- Medium/8. String to Integer (atoi).py
+++ Medium/8. String to Integer (atoi).py
@@ -1,61 +1,37 @@
-class OldSolution:
-    def myAtoi(self, s: str) -> int:
-        s = s.lstrip()
-        if not s:
-            return 0
-        nums = '0123456789'
-        negative = False
-        if s[0] == '+' or s[0] == '-':
-            if s[0] == '-':
-                negative = True
-            s = s[1:]
-        elif not(s[0] in nums):
-            return 0
-
-        retVal = 0
-        for char in s:
-            nums_i = nums.find(char)
-            if nums_i == -1:
-                break
-            else:
-                retVal *= 10
-                retVal += nums_i
-        if negative:
-            retVal = -retVal
-        if retVal > 2**31 - 1:
-            retVal = 2**31-1
-        if retVal < -(2**31):
-            retVal = -(2**31)
-        return retVal
-
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = list(s)
-        s.append('$')
         i = 0
-        while s[i] == ' ':
+        # ignore starting white space
+        while i < len(s) and s[i] == ' ':
             i += 1
 
+        # return 0 in case empty
+        if i == len(s):
+            return 0
+
+        # check if negative
         negative = s[i] == '-'
+
+        # "ignore" starting positive/negative sign
         if s[i] == '+' or s[i] == '-':
             i += 1
-    
-        zero_ord = ord('0')
-        nine_ord = ord('9')
-        res = 0
-        digit_ord = ord(s[i])
-        while digit_ord >= zero_ord and digit_ord <= nine_ord:
-            res *= 10
-            res += digit_ord - zero_ord
-            i += 1
-            digit_ord = ord(s[i])
 
+        # store ord of 0 for performance
+        zero_ord = ord('0')
+        res = 0
+        # go through string while current char is a digit
+        for char in s[i:]:
+            if char < '0' or char > '9':
+                break
+            res = res * 10 + (ord(char) - zero_ord)
+
+        # cap the result
         if negative:
             res = -res
-            if res < -(1<<31):
-                res = -(1<<31)
+            if res < -(1 << 31):
+                res = -(1 << 31)
         else:
-            if res > (1<<31) - 1:
-                res = (1<<31) - 1
-                
+            if res > (1 << 31) - 1:
+                res = (1 << 31) - 1
+
         return res
