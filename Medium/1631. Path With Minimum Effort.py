0676489103732d105b5ed1fa8686bffe3db515ea
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-04-28 11:19:41 +0200

    [1631.] Updated

--- Medium/1631. Path With Minimum Effort.py
+++ Medium/1631. Path With Minimum Effort.py
@@ -4,22 +4,31 @@ from typing import List
 
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        n = len(heights[0])
-        m = len(heights)
-        n_dec = n - 1
-        m_dec = m - 1
+        n_dec = len(heights[0]) - 1
+        m_dec = len(heights) - 1
 
-        # constraint: 1 <= heights[i][j] <= 10^6
-        effort_to_coords = [[1_000_001] * n for _ in range(m)]
+        # create visited structure
+        visited = [[False] * (n_dec + 1) for _ in range(m_dec + 1)]
 
         # min heap with values (effort_to_square, x, y)
         min_heap = [(0, 0, 0)]
 
-        # left, right, down, up deltas for x and y
-        deltas = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        while min_heap:
+            # get the square with the lowest effort from the heap
+            effort, x, y = heapq.heappop(min_heap)
 
-        # function to get in bound neighbors of coordinates
-        def get_valid_neighbors(x, y):
+            # if already visited, effort is larger/equal than any previous effort at these coordinates
+            if visited[y][x]:
+                continue
+
+            # if arrived at bottom coordinates, return effort, guaranteed to be lowest possible due to min heap
+            if x == n_dec and y == m_dec:
+                return effort
+
+            # update visited
+            visited[y][x] = True
+
+            # get all valid neighbors of current coordinate
             neighbors = []
             if x > 0:
                 neighbors.append((x - 1, y))
@@ -29,31 +38,13 @@ class Solution:
                 neighbors.append((x, y - 1))
             if y < m_dec:
                 neighbors.append((x, y + 1))
-            return neighbors
-
-        while min_heap:
-            # get the squre with the lowest effort from the heap
-            effort, x, y = heapq.heappop(min_heap)
-
-            # effort_to_coords changed in the mean time, effort could be larger, if so, skip
-            if effort >= effort_to_coords[y][x]:
-                continue
-
-            # if arrived at bottom coordinates, return effort, guaranteed to be lowest possible due to min heap
-            if x == n_dec and y == m_dec:
-                return effort
-
-            # update lowest possible effort to coordinates
-            effort_to_coords[y][x] = effort
 
             # store current height in local variable for performance
             curr_height = heights[y][x]
 
-            # go through all possible neighbors of current coordinate
-            for new_x, new_y in get_valid_neighbors(x, y):
-                # if the neighbor's coordinates are in bounds, calculate the effort to get there
-                effort_to_next = max(effort, abs(curr_height - heights[new_y][new_x]))
-
-                # if its lower than the current effort to get there, add it to the min heap
-                if (effort_to_next < effort_to_coords[new_y][new_x]):
+            # try traversing to each neighbor
+            for new_x, new_y in neighbors:
+                if not visited[new_y][new_x]:
+                    # calculate the effort to get to neighbor, and add to min heap
+                    effort_to_next = max(effort, abs(curr_height - heights[new_y][new_x]))
                     heapq.heappush(min_heap,  (effort_to_next, new_x, new_y))
