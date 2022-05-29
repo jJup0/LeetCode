abf24ddd25ead68cdff22025eaada312bb690f9f
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-05-29 18:09:53 +0200

    [268.] Solved

---
+++ Easy/268. Missing Number.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    """
+    Given an array nums containing n distinct numbers in the range [0, n], return the only number
+    in the range that is missing from the array.
+    Constraints:
+        n == nums.length
+        1 <= n <= 104
+        0 <= nums[i] <= n
+        All the numbers of nums are unique.
+    """
+
+    def missingNumber(self, nums: List[int]) -> int:
+        # this sum of all numbers from 1 to n equals n*(n+1)//2
+        n = len(nums)
+        gauss_sum = n*(n+1)//2
+        # subtract actual sum from theoretical sum to find missing number
+        return gauss_sum - sum(nums)
