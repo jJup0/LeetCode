0701e7e11d735dcbf837d17fb3847601a4462907
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-06-03 13:26:41 +0200

    [867.] Solved

---
+++ Easy/867. Transpose Matrix.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    """
+    Given a 2D integer array matrix, return the transpose of matrix.
+    The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+    Constraints:
+        m == matrix.length
+        n == matrix[i].length
+        1 <= m, n <= 1000
+        1 <= m * n <= 105
+        -109 <= matrix[i][j] <= 109
+    """
+
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        # construct a single row by iterating over rows in inner loop, and columns in outer loop
+        return [[matrix[i][j] for i in range(len(matrix))]
+                for j in range(len(matrix[0]))]
