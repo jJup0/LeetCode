c199114562185285816381b4bd06750219d6db7c
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-06-10 12:33:12 +0200

    [3.] Updated

--- Medium/3. Longest Substring Without Repeating Characters.py
+++ Medium/3. Longest Substring Without Repeating Characters.py
@@ -1,39 +1,34 @@
-class firstSolution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
-        longest = 1
-        curStart = curEnd = 0
-        while curEnd < len(s) - 1:
-            if s[curStart: curEnd + 1].rfind(s[curEnd + 1]) == -1:
-                curEnd += 1
-            else:
-                if curEnd - curStart + 1 > longest:
-                    longest = curEnd - curStart + 1
-                if s[curEnd] == s[curEnd + 1]:
-                    curStart = curEnd = curEnd + 1
-                else:
-                    curStart += 1
-        if curEnd - curStart + 1 > longest:
-            longest = curEnd - curStart + 1
-        return longest
-
-
 class Solution:
+    """
+    Given a string s, find the length of the longest substring without repeating characters.
+    Constraints:
+        0 <= s.length <= 5 * 10^4
+        s consists of English letters, digits, symbols and spaces.
+    """
+
     def lengthOfLongestSubstring(self, s: str) -> int:
-        lastOccurance = {}
+        # map last occurance of character
+        last_seen = {}
 
-        max_length = start = 0
+        # current start index of duplicate free substring
+        start = 0
 
-        for i, char in enumerate(s):
+        # result variable
+        res = 0
 
-            # next letter in question has already occured and current streak started before last occurance -> current occurence ruins streak
-            if char in lastOccurance and start <= lastOccurance[char]:
-                start = lastOccurance[char] + 1  # start new streak
-            else:
-                # check if new max streak
-                max_length = max(max_length, i - start + 1)
+        for i, c in enumerate(s):
+            # if character has already been seen and is part of current substring
+            if c in last_seen and last_seen[c] >= start:
+                # update res if necessary
+                curr_substring_length = i - start
+                if (curr_substring_length > res):
+                    res = curr_substring_length
 
-            # set last occurance of character to current i (place in string)
-            lastOccurance[char] = i
-        return max_length
+                # increment starting index by one to have duplicate free substring
+                start = last_seen[c] + 1
+
+            # update last occurance of c
+            last_seen[c] = i
+
+        # return maximum between previous longest substring and current
+        return max(res, len(s) - start)
