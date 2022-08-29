c24a0cd5e171247e3b440d8180ef29e65526952b
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-08-29 11:33:28 +0200

    [200.] Solved

---
+++ Medium/200. Number of Islands.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    """
+    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+    return the number of islands.
+    An island is surrounded by water and is formed by connecting adjacent lands horizontally or
+    vertically. You may assume all four edges of the grid are all surrounded by water.
+    """
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        O(n * m) / O(1)     time / space complexity
+        """
+        def checkNeighbours(row, col):
+            # dfs search in grid for "1" fills with "0" after visiting cell
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == "0":
+                return
+            grid[row][col] = "0"
+            checkNeighbours(row+1, col)
+            checkNeighbours(row, col+1)
+            checkNeighbours(row-1, col)
+            checkNeighbours(row, col-1)
+
+        # result variable
+        island_count = 0
+        # go through each cell checking if there is an unexplored piece of island
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                # if there is, explore it, and increase island count
+                if grid[row][col] == "1":
+                    checkNeighbours(row, col)
+                    island_count += 1
+
+        return island_count
