6df6f6e2c0dd22600649c57a8a9bfbad07aee3ce
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-08-30 08:42:15 +0200

    [48.] Updated

--- Medium/48. Rotate Image.py
+++ Medium/48. Rotate Image.py
@@ -2,20 +2,35 @@ from typing import List
 
 
 class Solution:
-    # in place
+    """
+    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees
+    (clockwise).
+    You have to rotate the image in-place, which means you have to modify the input 2D matrix
+    directly. DO NOT allocate another 2D matrix and do the rotation.
+    """
+
     def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        O(n^2) / O(1)     time / space complexity
+        """
         n_dec = len(matrix) - 1
 
-        # image of size n has n//2 "rings"
+        # rotate one ring at a time, image of size n has n//2 "rings"
         for ring in range(len(matrix)//2):
             # a ring can be broken up into 4 segments, each of length n - 1 - ring_idx * 2
             for step in range(ring, n_dec-ring):
-                left_top = matrix[ring][step]
-                right_top = matrix[step][n_dec-ring]
-                right_bottom = matrix[n_dec-ring][n_dec-step]
-                left_bottom = matrix[n_dec-step][ring]
+                # temp store top left cell
+                top_left = matrix[ring][step]
+                
+                # bottom left to top left
+                matrix[ring][step] = matrix[n_dec-step][ring]
+                
+                # bottom right to bottom left
+                matrix[n_dec-step][ring] = matrix[n_dec-ring][n_dec-step] 
 
-                matrix[ring][step] = left_bottom
-                matrix[step][n_dec-ring] = left_top
-                matrix[n_dec-ring][n_dec-step] = right_top
-                matrix[n_dec-step][ring] = right_bottom
+                # top right to bottom right
+                matrix[n_dec-ring][n_dec-step]= matrix[step][n_dec-ring]
+
+                # top left to bottom right
+                matrix[step][n_dec-ring] = top_left
+
