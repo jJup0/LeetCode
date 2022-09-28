646ecd87117f381ffcfcd7937b625d371dd14450
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-09-28 09:11:52 +0200

    [19.] Updated

--- Medium/19. Remove Nth Node From End of List.py
+++ Medium/19. Remove Nth Node From End of List.py
@@ -1,24 +1,46 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 
 class Solution:
+    """
+    Given the head of a linked list, remove the nth node from the end of the list and return its
+    head.
+
+    Constraints:
+        The number of nodes in the list is sz.
+        1 <= sz <= 30
+        0 <= Node.val <= 100
+        1 <= n <= sz
+    """
+
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        if not head.next:
-            return
-        count = 0
-        curNode = head
-        savedNode = head
-        while curNode.next:
-            if count > n-1:
-                savedNode = savedNode.next
-            curNode = curNode.next
-            count += 1
-        if count <= n-1:
-            return savedNode.next
-        else:
-            savedNode.next = savedNode.next.next
+        """
+        One pass using two pointers, one with n steps delay.
+        O(len(head)) / O(1)     time / space complexity
+        """
+        # skip the first n nodes
+        curr = head
+        for _ in range(n):
+            curr = curr.next
+
+        # if n == len(head) then curr == None, and we remove the first element
+        if not curr:
+            return head.next
+
+        # create a pointer to the predecessor of the node to be removed, this pointer will always
+        # point to n nodes to the left of curr, so when curr is pointing to the last node,
+        # to_remove_pred will point to the (n+1)th last node
+        to_remove_pred = head
+
+        # keep walking down the list until curr is last node in the list
+        while curr.next:
+            curr = curr.next
+            to_remove_pred = to_remove_pred.next
+
+        # remove the nth node
+        to_remove_pred.next = to_remove_pred.next.next
         return head
