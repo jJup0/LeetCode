547c0620b03cd1addfbbac9092b87c5832ba921e
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-11-11 08:56:20 +0100

    [26.] Updated

--- Easy/26. Remove Duplicates from Sorted Array.py
+++ Easy/26. Remove Duplicates from Sorted Array.py
@@ -1,55 +1,59 @@
-class firstSolution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not len(nums):
-            return 0
-        prev = nums[0]
-        i = 1
-        while i < len(nums):
-            if nums[i] == prev:
-                del nums[i]
-            else:
-                prev = nums[i]
-                i += 1
-        return len(nums)
-
-
-class SolutionIdk(object):  # duplicates are replaced, because actual return is the first next_i_to_replace'th values in nums
-    def removeDuplicates(self, nums):
-        if len(nums) == 0:
-            return 0
-        prev_n = nums[0]
-        next_i_to_replace = 1
-        for i in range(len(nums)):
-            if nums[i] != prev_n:
-                nums[next_i_to_replace] = nums[i]
-                next_i_to_replace += 1
-                prev_n = nums[i]
-        return next_i_to_replace
-
-class delSolution:
-    def removeDuplicates(self, nums: [int]) -> int:
-        lastnum = None
-        i = 0
-        while i < len(nums):
-            if nums[i] == lastnum:
-                del nums[i]
-            else:
-                i += 1
-                lastnum = nums[i]
-        return len(nums)
+from typing import List
 
 
 class Solution:
-    def removeDuplicates(self, nums: [int]) -> int:
-        uniquePos = 0
-        for curPos in range(1, len(nums)):
-            if nums[uniquePos] != nums[curPos]:
-                curPos += 1
-                nums[uniquePos] = nums[curPos]
+    """
+    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such
+    that each unique element appears only once. The relative order of the elements should be kept
+    the same.
 
-        return uniquePos+1
+    Since it is impossible to change the length of the array in some languages, you must instead
+    have the result be placed in the first part of the array nums. More formally, if there are k
+    elements after removing the duplicates, then the first k elements of nums should hold the final
+    result. It does not matter what you leave beyond the first k elements.
 
+    Return k after placing the final result in the first k slots of nums.
+
+    Do not allocate extra space for another array. You must do this by modifying the input array
+    in-place with O(1) extra memory.
+
+    Custom Judge:
+
+    The judge will test your solution with the following code:
+
+        int[] nums = [...]; // Input array
+        int[] expectedNums = [...]; // The expected answer with correct length
+
+        int k = removeDuplicates(nums); // Calls your implementation
+
+        assert k == expectedNums.length;
+        for (int i = 0; i < k; i++) {
+            assert nums[i] == expectedNums[i];
+        }
+    If all assertions pass, then your solution will be accepted.
+
+    Constraints:
+        1 <= nums.length <= 3 * 10^4
+        -100 <= nums[i] <= 100
+        nums is sorted in non-decreasing order.
+    """
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        O(n) / O(1)     time / space complexity
+        """
+        
+        # previous number
+        prev = None
+        
+        # total amount of unique values so far, also index for where next unique value will be
+        # placed
+        res = 0
+        
+        for num in nums:
+            # if num is unique, or first of its value replace previous duplicate and update res
+            if num != prev:
+                nums[res] = num
+                res += 1
+            prev = num
+        return res
