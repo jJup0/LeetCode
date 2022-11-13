88fe789444a481a71c3d5d7879cb70fe70713f34
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-11-13 23:35:10 +0100

    [151.] Updated

--- Medium/151. Reverse Words in a String.py
+++ Medium/151. Reverse Words in a String.py
@@ -1,13 +1,49 @@
+from typing import List
+
+
 class Solution:
+    """
+    Given an input string s, reverse the order of the words.
+
+    A word is defined as a sequence of non-space characters. The words in s will be separated by at
+    least one space.
+
+    Return a string of the words in reverse order concatenated by a single space.
+
+    Note that s may contain leading or trailing spaces or multiple spaces between two words. The
+    returned string should only have a single space separating the words. Do not include any extra
+    spaces.
+
+    Constraints:
+        1 <= s.length <= 10^4
+        s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+        There is at least one word in s.
+    """
+
     def reverseWords(self, s: str) -> str:
         return " ".join(reversed(s.split()))
-        # s += ' '
-        # curWord = ''
-        # retStr = ''
-        # for c in s:
-        #     if c != ' ':
-        #         curWord += c
-        #     elif curWord:
-        #         retStr = ' ' + curWord + retStr
-        #         curWord = ''
-        # return retStr[1:]
+
+    def followUp(self, s: List[str]):
+        # Follow-up: If the string data type is mutable in your language, can you solve it in-place
+        # with O(1) extra space?
+        def reverse_word(start, stop):
+            half_word_len = ((stop - start) // 2)
+            for j in range(half_word_len + 1):
+                s[start + j], s[stop - j] = s[stop - j], s[start + j]
+        
+        first_char_idx = 0
+        for i in range(len(s)):
+            if s[i] == ' ':
+                reverse_word(first_char_idx, i - 1)
+                first_char_idx = i + 1
+
+        reverse_word(first_char_idx, len(s) - 1)
+
+        # remore duplicate space
+        last_free_idx = 1
+        for i in range(1, len(s)):
+            if s[i] != ' ' or s[i - 1] != ' ':
+                s[last_free_idx] = s[i]
+                last_free_idx += 1
+        
+        return s[:last_free_idx]
--- test_data_generator.py
+++ test_data_generator.py
@@ -1,20 +1,20 @@
 from random import randint as rint
+from typing import Any, List, Tuple
 
 import pyperclip
 
 
 class TestData:
-    
+
     def __init__(self, test_cases, count=10):
         self.test_cases = test_cases
         self.case_count = count
-    
+
     def clear_test_cases(self):
         self.test_cases.clear()
 
     def set_test_case_count(self, count):
         self.case_count = count
-
 
     def int_couple_list(self, min_val, max_val, size):
         l = []
@@ -23,14 +23,12 @@ class TestData:
 
         self.test_cases.append(str(l))
 
-
     def int_list(self, min_val, max_val, size):
         l = []
         for _ in range(size):
             l.append(rint(min_val, max_val))
 
         self.test_cases.append(str(l))
-
 
     def almost_non_decreasing_int_list(self, size):
         l = list(range(size))
@@ -40,7 +38,6 @@ class TestData:
 
         self.test_cases.append(str(l))
 
-
     def two_rects(self):
         for _ in range(2):
             self.test_cases.append(str(rint(-10, 0)))
@@ -48,10 +45,8 @@ class TestData:
             self.test_cases.append(str(rint(0, 10)))
             self.test_cases.append(str(rint(0, 10)))
 
-
     def digits_str(self, size):
         self.test_cases.append('"' + ''.join(str(rint(0, 9)) for _ in range(size)) + '"')
-
 
     def similar_words_list(self, line_count, word_count, max_word_len):
         for _ in range(line_count):
@@ -69,6 +64,43 @@ class TestData:
             l.append([left, right, height])
         l.sort()
         self.test_cases.append(str(l))
+
+
+class ComplexStructureTestData:
+    def median_from_data_stream(self, calls: int) -> Tuple[List[str], List[List[int]]]:
+        methods = ["MedianFinder"] + ["addNum", "findMedian"] * calls
+        paramss = [[]]
+        for _ in range(calls):
+            paramss.append([rint(-100_000, 100_000)])
+            paramss.append([])
+
+        return methods, paramss
+
+    def generate_and_copy_to_clip(self, calls):
+        methods, paramss = self.median_from_data_stream(1)
+
+        mstr = str(methods).replace(" ", "").replace("'", '"')
+        pstr = str(paramss).replace(" ", "").replace("'", '"')
+        s = mstr + "\n" + pstr
+        pyperclip.copy(s)
+        print(s)
+        print("^ copied to clipboard")
+
+        return methods, paramss
+
+
+    def evaluate(self, methods, paramss):
+        print(methods, paramss)
+        for method, params in zip(methods, paramss):
+            if method == "MedianFinder":
+                # S = MedianFinder(*params)
+                pass
+            else:
+                # res = eval(f"S.{method}({','.join(str(param) for param in params)})")
+                # if res:
+                # print(res)
+                pass
+
 
 if __name__ == "__main__":
     test_cases = []
