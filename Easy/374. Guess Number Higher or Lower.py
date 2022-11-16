9dd0332a7f04d718ad942219e2243f8dc9652cc5
Jakob Roithinger <e52009269@student.tuwien.ac.at>
2022-11-16 08:24:49 +0100

    [374.] Updated

--- Easy/374. Guess Number Higher or Lower.py
+++ Easy/374. Guess Number Higher or Lower.py
@@ -2,15 +2,40 @@
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 # def guess(num: int) -> int:
-def guess(val):
-    valToGuess = 1
-    if val == valToGuess:
-        return 0
-    return 1 if val > valToGuess else -1
+
+# def guess(val: int) -> int:
+#     valToGuess = #a number
+#     if val == valToGuess:
+#         return 0
+#     return 1 if val > valToGuess else -1
 
 
 class Solution:
+    """
+    We are playing the Guess Game. The game is as follows:
+
+    I pick a number from 1 to n. You have to guess which number I picked.
+
+    Every time you guess wrong, I will tell you whether the number I picked is higher or lower than
+    your guess.
+
+    You call a pre-defined API int guess(int num), which returns three possible results:
+        -1: Your guess is higher than the number I picked (i.e. num > pick).
+        1: Your guess is lower than the number I picked (i.e. num < pick).
+        0: your guess is equal to the number I picked (i.e. num == pick).
+
+    Return the number that I picked.
+
+    Constraints:
+        1 <= n <= 2^31 - 1
+        1 <= pick <= n
+    """
+
     def guessNumber(self, n: int) -> int:
+        """
+        Simple binary serach approach
+        O(log(n)) / O(1)    time / space complexity
+        """
         lo = 0
         hi = n
         res = g = -2
