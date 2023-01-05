b3b738266f407772b0d6fcb74017b341bf1530eb
Jakob Roithinger <jake.roithinger@gmail.com>
2023-01-05 09:34:37 +0100

    [452.] Updated

--- Medium/452. Minimum Number of Arrows to Burst Balloons.py
+++ Medium/452. Minimum Number of Arrows to Burst Balloons.py
@@ -1,21 +1,46 @@
 class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key = lambda x : x[1])
-        # ret = 0
-        # i = 0
-        # while i < len(points):
-        #     _, x_end = points[i]
-        #     i += 1
-        #     while i < len(points) and points[i][0] <= x_end:
-        #         i += 1
-        #     ret += 1
-        # return ret
-        
-        # same concept, but faster and cleaner
-        res = 1
-        tail = points[0][1]
-        for i in points[1:]:
-            if i[0] > tail:
+    """
+    There are some spherical balloons taped onto a flat wall that represents the XY-plane. The
+    balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes
+    a balloon whose horizontal diameter stretches between xstart and xend. You do not know the
+    exact y-coordinates of the balloons.
+
+    Arrows can be shot up directly vertically (in the positive y-direction) from different points
+    along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if
+    xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow
+    keeps traveling up infinitely, bursting any balloons in its path.
+
+    Given the array points, return the minimum number of arrows that must be shot to burst all
+    balloons.
+
+    Constraints:
+        1 <= points.length <= 10^5
+        points[i].length == 2
+        -2^31 <= xstart < xend <= 2^31 - 1
+    """
+
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        """
+        Sort balloons by end coordinate and shoot arrow at x_end coordinate. The "first ending"
+        balloon needs to have an arrow shot through it, so shoot arrow at its ending coordinate,
+        to maximize hits on other ballons. Find next "first ending" unshot balloon and repeat.
+
+        O(n * log(n)) / O(1)    time / space complexity
+        """
+
+        # sort balloons by end coordinate
+        points.sort(key=lambda x: x[1])
+
+        # result variable
+        res = 0
+
+        # x coordinate of last arrow shot
+        prev_arrow = float('-inf')
+
+        for start, end in points:
+            # if arrow not yet shot, shoot arrow at its ending coordinates
+            if start > prev_arrow:
                 res += 1
-                tail = i[1]
+                prev_arrow = end
+
         return res
