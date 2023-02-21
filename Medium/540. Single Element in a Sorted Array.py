bb58e827300ea982ed83a8cd6ca01e4181ce6496
Jakob Roithinger <jake.roithinger@gmail.com>
2023-02-21 15:57:27 +0100

    [540.] Updated

--- Medium/540. Single Element in a Sorted Array.py
+++ Medium/540. Single Element in a Sorted Array.py
@@ -1,14 +1,38 @@
 class Solution:
-    def singleNonDuplicate(self, nums: [int]) -> int:
-        low, high = 0, len(nums)-1
-        while high > low:
-            mid = (high + low) >> 1
-            # if at "odd" index, since 0 indexed, but technically even index, decrement
-            if mid % 2:
+    """
+    You are given a sorted array consisting of only integers where every element appears exactly
+    twice, except for one element which appears exactly once.
+
+    Return the single element that appears only once.
+
+    Your solution must run in O(log n) time and O(1) space.
+
+    Constraints:
+        1 <= nums.length <= 10^5
+        0 <= nums[i] <= 10^5
+    """
+
+    def singleNonDuplicate(self, nums: list[int]) -> int:
+        """
+        "Normal" binary search problem. If even middle value and its successor are the same value
+        then single value is between mid + 2 and high, else between low and mid.
+        """
+
+        low = 0
+        high = len(nums) - 1
+        # maintain loop invariant that the single value is in nums[lo:hi+1]
+        while low < high:
+            mid = (low + high) >> 1
+
+            # make sure mid is even
+            if mid & 1:
                 mid -= 1
-            # if numbers next to each other are the same, then the single number is further ahead
-            if nums[mid] != nums[mid+1]:
-                high = mid
-            else:
+
+            if nums[mid] == nums[mid + 1]:
+                # single value is at least at index mid + 2
                 low = mid + 2
-        return nums[high]
+            else:
+                # single value is at most at mid
+                high = mid
+
+        return nums[low]
