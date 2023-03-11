4cfa6dfa8088b07c6b6cc4b4080e433dd2b37e35
Jakob Roithinger <jake.roithinger@gmail.com>
2023-03-11 08:30:37 +0100

    [109.] Updated

--- Medium/109. Convert Sorted List to Binary Search Tree.py
+++ Medium/109. Convert Sorted List to Binary Search Tree.py
@@ -1,67 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from typing import Optional
 
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
-    """
-    Given the head of a singly linked list where elements are sorted in ascending order, convert it
-    to a height balanced BST.
-    For this problem, a height-balanced binary tree is defined as a binary tree in which the depth
-    of the two subtrees of every node never differ by more than 1.
-    Constraints:
-        The number of nodes in head is in the range [0, 2 * 10^4].
-        -10^5 <= Node.val <= 10^5
-    """
-
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        """Only determines length of list, then calls helper method
+        O(n * log(n)) / O(log(n))    time / auxiliary space complexity
         """
-        Construct balanced tree by always splitting list in half.
+        # find out length of list
+        length = 0
+        len_node = head
+        while len_node:
+            len_node = len_node.next
+            length += 1
+
+        return self._sorted_list_length_to_BST(head, length)
+
+    def _sorted_list_length_to_BST(self, list_root: Optional[ListNode], length: int):
+        """Recursively splits list in half to generate tree child nodes.
+        O(n * log(n)) / O(log(n))   time / auxiliary space complexity
         """
-        if not head:  # if no nodes, return null
+        # Base cases: if list is empty return None,
+        # is list has one element return TreeNode with its value
+        if length == 0:
             return None
-        if not head.next:  # if only one node, return as TreeNode
-            return TreeNode(head.val)
+        if length == 1:
+            return TreeNode(list_root.val)
 
-        # find node before middle node with slow/fast method and initializing fast to head.next.next
-        slow = head
-        fast = head.next.next
-        while fast and fast.next:
-            assert slow  # type hint
-            slow = slow.next
-            fast = fast.next.next
+        # traverse to middle node
+        half_len = length // 2
+        middle_node = list_root
+        for _ in range(half_len):
+            middle_node = middle_node.next
 
-        assert slow  # type hint
-
-        # keep reference to middle node
-        middle = slow.next
-
-        assert middle  # type hint
-
-        # cut of start of list from middle
-        slow.next = None
-
-        # initialize tree with middle value
-        root = TreeNode(middle.val)
-
-        # construct left children with all values less than middle
-        root.left = self.sortedListToBST(head)
-
-        # construct right children with all values greater than middle
-        root.right = self.sortedListToBST(middle.next)
-
-        # return constructed tree
-        return root
+        # generate left and right child trees
+        # left child is created from list[:mid], right child from list[mid+1:]
+        left_child = self._sorted_list_length_to_BST(list_root, half_len)
+        right_child = self._sorted_list_length_to_BST(middle_node.next, length - half_len - 1)
+        return TreeNode(middle_node.val, left_child, right_child)
