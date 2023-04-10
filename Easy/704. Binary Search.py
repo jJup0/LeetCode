a24ee4ba1fc40b0e51645942340d89060c4fa938
Jakob Roithinger <jake.roithinger@gmail.com>
2023-04-10 17:17:32 +0200

    [704.] Updated

--- Easy/704. Binary Search.py
+++ Easy/704. Binary Search.py
@@ -1,7 +1,22 @@
 from typing import List
+
+
 class Solution:
-    # constraint nums is sorted
-    def search(self, nums: List[int], target: int) -> int:
+    """
+    Given an array of integers nums which is sorted in ascending order, and an
+    integer target, write a function to search target in nums. If target exists,
+    then return its index. Otherwise, return -1.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Constraints:
+        1 <= nums.length <= 10^4
+        -10^4 < nums[i], target < 10^4
+        All the integers in nums are unique.
+        nums is sorted in ascending order.
+    """
+
+    def search(self, nums: list[int], target: int) -> int:
         # lo and hi represent lower and higher boundary of where the number could be
         lo = 0
         hi = len(nums)
@@ -10,14 +25,15 @@ class Solution:
             # calculate the middle position between the boundaries
             mid = (lo + hi) >> 1
             # if the number at that position is smaller, then set lo as mid + 1, as any number at a lower
-            # index than mid can not be the target, as nums[mid] is already too small 
+            # index than mid can not be the target, as nums[mid] is already too small
             if nums[mid] < target:
                 lo = mid + 1
             else:
                 # else set high as the new middle, do not subtract 1, because here nums[mid] could be equal to target
                 hi = mid
-            
-        # assert lo == hi
+
         # if lo/hi is in bounds of the array and the number at nums[lo] is the target return the index
+        if lo < len(nums) and nums[lo] == target:
+            return lo
         # otherwise the number is not in the array so return -1
-        return lo if (lo < len(nums) and nums[lo] == target) else -1
+        return -1
