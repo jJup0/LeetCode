290f6a2adb3fc842f42998916bd08217bccf4d9f
Jakob Roithinger <jake.roithinger@gmail.com>
2023-05-16 07:41:46 +0200

    [59.] Updated

--- Medium/59. Spiral Matrix II.py
+++ Medium/59. Spiral Matrix II.py
@@ -1,10 +1,14 @@
-from typing import List
-
-
 class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
+    """
+    Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+
+    Constraints:
+        1 <= n <= 20
+    """
+
+    def generateMatrix(self, n: int) -> list[list[int]]:
         # initliaze matrix structure
-        res = [[0]*n for _ in range(n)]
+        res = [[0] * n for _ in range(n)]
 
         # next value to fill into matrix
         val = 1
@@ -13,29 +17,29 @@ class Solution:
         x = 0
 
         # generate the matrix in rings, matrix has ceil(n/2) rings
-        for ring in range((n//2)+1):
+        for ring in range((n // 2) + 1):
             # start y at level of ring
             y = ring
             # generate top row going from (ring, ring) going until (n-1-ring, ring)
-            for x in range(ring, n-ring):
+            for x in range(ring, n - ring):
                 res[y][x] = val
                 val += 1
 
             # generate right column keeping x-value from before,
             # going from (n-1-ring, ring+1) to (n-1-ring, n-1-ring)
-            for y in range(y+1, n-ring):
+            for y in range(y + 1, n - ring):
                 res[y][x] = val
                 val += 1
 
             # generate bottom row keeping y-value from before
             # going from (n-1-ring-1,  n-1-ring) to (ring, n-1-ring)
-            for x in range(x-1, ring-1, -1):
+            for x in range(x - 1, ring - 1, -1):
                 res[y][x] = val
                 val += 1
 
             # generate left column keeping x-value from before
             # going from (ring, n-1-ring-1) to (ring, ring-1)
-            for y in range(y-1, ring, -1):
+            for y in range(y - 1, ring, -1):
                 res[y][x] = val
                 val += 1
         return res
