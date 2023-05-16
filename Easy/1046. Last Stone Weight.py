0da1ac8339429f894aa488ef9b992b3584c7e065
Jakob Roithinger <jake.roithinger@gmail.com>
2023-05-16 07:28:56 +0200

    [1046.] Updated

--- Easy/1046. Last Stone Weight.py
+++ Easy/1046. Last Stone Weight.py
@@ -1,9 +1,28 @@
 import heapq
-from typing import List
 
 
 class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
+    """
+    You are given an array of integers stones where stones[i] is the weight of the
+    ith stone.
+
+    We are playing a game with the stones. On each turn, we choose the heaviest two
+    stones and smash them together. Suppose the heaviest two stones have weights
+    x and y with x <= y. The result of this smash is:
+        If x == y, both stones are destroyed, and
+        If x != y, the stone of weight x is destroyed, and the stone of weight y
+          has new weight y - x.
+
+    At the end of the game, there is at most one stone left.
+
+    Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+    Constraints:
+        1 <= stones.length <= 30
+        1 <= stones[i] <= 1000
+    """
+
+    def lastStoneWeight(self, stones: list[int]) -> int:
         # create a max heap for the stones, possible by negating values
         # and creating a min heap from these values
         stones = list(-x for x in stones)
