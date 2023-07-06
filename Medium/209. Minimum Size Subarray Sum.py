56b3ae79cf18336fce57df157c6a94b5605a2fb9
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-06 06:18:55 +0200

    [209.] Updated

--- Medium/209. Minimum Size Subarray Sum.py
+++ Medium/209. Minimum Size Subarray Sum.py
@@ -1,21 +1,24 @@
-from typing import List
-
-
 class Solution:
     """
-    Given an array of positive integers nums and a positive integer target, return the minimal
-    length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is
-    greater than or equal to target. If there is no such subarray, return 0 instead.
+    Given an array of positive integers nums and a positive integer target,
+    return the minimal length of a contiguous subarray [numsl, numsl+1, ...,
+    numsr-1, numsr] of which the sum is greater than or equal to target. If
+    there is no such subarray, return 0 instead.
+
     Constraints:
-        1 <= target <= 10^9
-        1 <= nums.length <= 10^5
-        1 <= nums[i] <= 10^4
+    - 1 <= target <= 10^9
+    - 1 <= nums.length <= 10^5
+    - 1 <= nums[i] <= 10^4
     """
 
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+        """Sliding window approach.
+
+        O(n) / O(1)     time / space complexity
+        """
         # start index of current sub array
         start = 0
-        
+
         # current running sum
         total = 0
 
@@ -25,12 +28,14 @@ class Solution:
         for end, num in enumerate(nums):
             # add current value to total
             total += num
-            # shrink sub array while total is greater equal to target
+            # shrink subarray while total is greater equal to target
             while total >= target:
                 # update res if smaller than previous
                 res = min(end - start + 1, res)
                 total -= nums[start]
                 start += 1
 
-        # if res has default value return 0, else res
-        return 0 if res == len(nums) + 1 else res
+        # if res has default value, then total sum is smallest than target
+        if res == len(nums) + 1:
+            return 0
+        return res
