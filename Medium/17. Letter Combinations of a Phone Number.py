83c9a7a26b7f3442d0b8297c8776fd9e55404496
Jakob Roithinger <jake.roithinger@gmail.com>
2023-08-03 15:33:12 +0200

    [17.] Updated

--- Medium/17. Letter Combinations of a Phone Number.py
+++ Medium/17. Letter Combinations of a Phone Number.py
@@ -4,8 +4,12 @@ from typing import List
 class Solution:
 
     """
-    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
-    A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+    Given a string containing digits from 2-9 inclusive, return all possible letter
+    combinations that the number could represent. Return the answer in any order.
+
+    A mapping of digit to letters (just like on the telephone buttons) is given
+    below. Note that 1 does not map to any letters.
+
     https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/200px-Telephone-keypad2.svg.png
 
     Constraints:
@@ -15,14 +19,16 @@ class Solution:
 
     def __init__(self):
         # define a mapping from numbers to possible letters
-        self.num2let = {'2': 'abc',
-                        '3': 'def',
-                        '4': 'ghi',
-                        '5': 'jkl',
-                        '6': 'mno',
-                        '7': 'pqrs',
-                        '8': 'tuv',
-                        '9': 'wxyz'}
+        self.num2let = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
 
     def letterCombinations(self, digits: str) -> List[str]:
         # if the string is empty, return empty list
@@ -35,7 +41,7 @@ class Solution:
         # go through rest of the letters, appending them to the previous combinations
         for char in digits[1:]:
             # store new combinations in separate list
-            new_combs = []
+            new_combs: list[str] = []
             # iterate through possible letters
             for char in self.num2let[char]:
                 # extends new_combs by possible iterators
