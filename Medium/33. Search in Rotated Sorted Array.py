ae49b666f70cbb773e7aa13c4356731e80e352ea
Jakob Roithinger <jake.roithinger@gmail.com>
2023-08-08 06:28:31 +0200

    [33.] Updated

--- Medium/33. Search in Rotated Sorted Array.py
+++ Medium/33. Search in Rotated Sorted Array.py
@@ -1,30 +1,54 @@
 class Solution:
-    def search(self, nums: [int], target: int) -> int:
-        idx = 0
-        while len(nums) > 0:
-            mid = len(nums) // 2
-            if nums[mid] <= nums[-1]: # pivot is in first half
-                if target >= nums[mid] and target <= nums[-1]: # check if the target is in the that half
-                    nums = nums[mid:]
-                    idx = idx+mid   #since nums is modified, add midpoint to index of the result
-                else: # if not, it must be in another half
-                    nums = nums[:mid]
-            else: # pivot is in second half
-                if target >= nums[0] and target <= nums[mid-1]: # check if the target is in the that half
-                    nums = nums[:mid]
-                else: # if not, it must be in another half
-                    nums = nums[mid:] 
-                    idx = idx+mid
-            if len(nums) == 1:
-                if nums[0] == target:
-                    return idx
-                else:
-                    return -1
-        return -1
+    """
+    There is an integer array nums sorted in ascending order (with distinct values).
 
- # since every other number appears exactly twice, by xoring, they will eventually all cancel each other out, even if in a weird order
-    def xorSingleNumber(self, nums: [int]) -> int:
-        number = 0
-        for i in nums:
-            number = number ^ i
-        return number
+    Prior to being passed to your function, nums is possibly rotated at an unknown
+    pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k],
+    nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+    For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become
+    [4,5,6,7,0,1,2].
+
+    Given the array nums after the possible rotation and an integer target, return
+    the index of target if it is in nums, or -1 if it is not in nums.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Constraints:
+    - 1 <= nums.length <= 5000
+    - -10^4 <= nums[i] <= 10^4
+    - All values of nums are unique.
+    - nums is an ascending array that is possibly rotated.
+    - -10^4 <= target <= 10^4
+    """
+
+    def search(self, nums: list[int], target: int) -> int:
+        """
+        At every iteration of the binary search, check in which half the rotation point is.
+        Technically do not need to check once nums[l:r] is known to be strictly increasing.
+
+        O(log(n)) / O(1)    time / space complexity
+        """
+
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) >> 1
+            if nums[mid] == target:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                # pivot is in second half
+                if nums[l] <= target < nums[mid]:
+                    # target is in non rotated first half
+                    r = mid - 1
+                else:
+                    # target is in rotated second half
+                    l = mid + 1
+            else:
+                # pivot is in first half
+                if nums[mid] < target <= nums[r]:
+                    # target is in non rotated second half
+                    l = mid + 1
+                else:
+                    # target is in rotated first half
+                    r = mid - 1
+        return -1
