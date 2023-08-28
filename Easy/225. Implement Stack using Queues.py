46d7d75e8592ef111570dc5528d2103114d95f57
Jakob Roithinger <jake.roithinger@gmail.com>
2023-08-28 06:42:21 +0200

    [225.] Updated

--- Easy/225. Implement Stack using Queues.py
+++ Easy/225. Implement Stack using Queues.py
@@ -5,11 +5,13 @@ class MyStack:
 
     """
     Implement a last-in-first-out (LIFO) stack using only two queues.
-    The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+    The implemented stack should support all the functions of a normal stack
+    (push, top, pop, and empty).
+
     Constraints:
-        1 <= x <= 9
-        At most 100 calls will be made to push, pop, top, and empty.
-        All the calls to pop and top are valid.
+    - 1 <= x <= 9
+    - At most 100 calls will be made to push, pop, top, and empty.
+    - All the calls to pop and top are valid.
     Follow-up:
         Can you implement the stack using only one queue?
     """
@@ -19,12 +21,11 @@ class MyStack:
         # using only append and popleft functions of deque to simulate queue
         # queue will always be in a state that resembles a stack in terms O(1) pop operations,
         # this way peek is also in O(1), as queue[-1] would not be allowed in this challenge
-        self.queue = deque()
+        self.queue: deque[int] = deque()
 
     # O(n)
     # append the item to the queue and then cycle all previous items to the back of the queue,
     # maintaining the stack property
-
     def push(self, x: int) -> None:
         n = len(self.queue)
         self.queue.append(x)
