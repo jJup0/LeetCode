3da66ea9b8103c44d3518c86e5263bc5ff52efa2
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-07 06:50:24 +0200

    [92.] Updated

--- Medium/92. Reverse Linked List II.py
+++ Medium/92. Reverse Linked List II.py
@@ -1,34 +1,46 @@
-from typing import Optional
+from typing import TYPE_CHECKING, cast
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+if TYPE_CHECKING:
+    # Definition for singly-linked list.
+    class ListNode:
+        def __init__(self, val: int = 0, next: "ListNode" | None = None):
+            self.val = val
+            self.next = next
 
 
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    """
+    Given the head of a singly linked list and two integers left and right where
+    left <= right, reverse the nodes of the list from position left to position
+    right, and return the reversed list.
+
+    Constraints:
+    - The number of nodes in the list is n.
+    - 1 <= n <= 500
+    - -500 <= Node.val <= 500
+    - 1 <= left <= right <= n
+    """
+
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
         # create a dummy node, in case left == 1
         dummy = curr = ListNode(next=head)
         # go to parent node of first node affected by reverse
-        for _ in range(left-1):
-            curr = curr.next
+        for _ in range(left - 1):
+            curr = cast(ListNode, curr.next)
 
-        # mark end of the unreversed part (append reversed part here later)
+        # store point to end of the first unreversed part (append reversed part here later)
         tail_of_in_unreversed = curr
-        # go one node forward, and cut off unreversed part from soon to be reversed part
-        curr = curr.next
-        tail_of_in_unreversed.next = None
+        
+        curr = cast(ListNode, curr.next)
         # the first node in the chain of nodes which is to be reversed will be the end of the reversed part
         reversed_tail = curr
         # head of the listnode of reversed part
         reversed_head = None
 
         # go through all nodes which are to be reversed
-        for _ in range(right-left+1):
+        for _ in range(right - left + 1):
             # temporarily store next node
-            temp = curr.next
+            temp = cast(ListNode, curr.next)
             # build reverse chain
             curr.next = reversed_head
             # set new head of reversed chain
@@ -41,5 +53,5 @@ class Solution:
         # append rest of list to reversed list
         reversed_tail.next = curr
 
-        # return head if left > 1, else new head of list
-        return dummy.next
+        # return head
+        return cast(ListNode, dummy.next)
