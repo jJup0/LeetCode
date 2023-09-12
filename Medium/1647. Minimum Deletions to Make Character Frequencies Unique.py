d36bd9d63ed153034ec39da7668b2b027abce526
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-12 06:37:37 +0200

    [1647.] Updated

--- Medium/1647. Minimum Deletions to Make Character Frequencies Unique.py
+++ Medium/1647. Minimum Deletions to Make Character Frequencies Unique.py
@@ -1,57 +1,56 @@
-from typing import Counter, List
+from collections import Counter
 
 
 class Solution:
     """
-    A string s is called good if there are no two different characters in s that have the same
-    frequency.
-    Given a string s, return the minimum number of characters you need to delete to make s good.
-    The frequency of a character in a string is the number of times it appears in the string.
-    For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+    A string s is called good if there are no two different characters in s that
+    have the same frequency.
+
+    Given a string s, return the minimum number of characters you need to delete
+    to make s good.
+
+    The frequency of a character in a string is the number of times it appears
+    in the string. For example, in the string "aab", the frequency of 'a' is 2,
+    while the frequency of 'b' is 1.
+
     Constraints:
-        1 <= s.length <= 10^5
-        s contains only lowercase English letters.
+    - 1 <= s.length <= 10^5
+    - s contains only lowercase English letters.
     """
 
     def minDeletions(self, s: str) -> int:
-        # result variable
-        res = 0
+        """
+        O(n) / O(n)     time / space complexity
+        """
+        # mapping from character occurence count to amount of characters
+        # that have that count
+        char_count_counter = Counter(Counter(s).values())
+        result = 0
+        # next largest count that is unique
+        next_largest_unique = len(s)
+        # iterate through counts, sorted by char count decresing
+        for char_count, count_count in sorted(char_count_counter.items(), reverse=True):
+            # if only one, then it is unique
+            if count_count == 1:
+                continue
 
-        # all counts for characters
-        counts = sorted(count for _, count in Counter(s).items())
+            # update next largest unique to minimum of itself and current char count-1,
+            # as we cannot add characters
+            next_largest_unique = min(next_largest_unique, char_count - 1)
 
-        # previous count
-        prev = 0
+            # while they count is not unique, and there are unique counts left
+            while count_count > 1 and next_largest_unique > 0:
+                if next_largest_unique not in char_count_counter:
+                    # if the char count that we want to use is unique,
+                    # remove characters until it is unique
+                    count_count -= 1
+                    result += char_count - next_largest_unique
 
-        # ranges of occurance count values that are not yet occupied
-        # use ranges instead of concrete count values, in case counts looks something like [1, 1000000000]
-        not_taken: List[List[int]] = []
-        for count in counts:
-            # if count is same as previous, need to removed letter
-            if count == prev:
-                # if there are occurances, 'delete' character until only not_taken[-1][1] characters remain
-                if not_taken:
-                    # get interval from not taken stack
-                    (start, end) = taken = not_taken[-1]
+                # in any case, next largest unique count must be decremented
+                next_largest_unique -= 1
 
-                    # 'delete' required characters
-                    res += count - end
+            # this line accounts for characters which can no longer have
+            # a unique count, they must be entirely removed
+            result += (count_count - 1) * (char_count - next_largest_unique)
 
-                    # reduce interval by one
-                    taken[1] -= 1
-
-                    # if size of interval is 0, delete it
-                    if start == taken[1]:
-                        not_taken.pop()
-                else:
-                    # if there are no vacant occurance counts, delete all occurances of character
-                    res += count
-            else:
-                # if counts are unequal
-                # if a non empty interval of occurances can be append to not taken, do so
-                if count - 1 > prev:
-                    not_taken.append([prev, count-1])
-                # update prev
-                prev = count
-
-        return res
+        return result
