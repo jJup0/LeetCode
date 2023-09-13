fc9b2ec3a505b34186602a32efaf95f8912453be
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-13 07:11:42 +0200

    [135.] Updated

--- Hard/135. Candy.py
+++ Hard/135. Candy.py
@@ -1,25 +1,26 @@
 from collections import defaultdict
-from typing import List
 
 
 class Solution:
     """
-    Given an unsorted array of integers nums, return the length of the longest consecutive
-    elements sequence.
+    Given an unsorted array of integers nums, return the length of the longest
+    consecutive elements sequence.
+
     You must write an algorithm that runs in O(n) time.
+
     Constraints:
-        0 <= nums.length <= 10^5
-        -10^9 <= nums[i] <= 10^9
+    - 0 <= nums.length <= 10^5
+    - -10^9 <= nums[i] <= 10^9
     """
 
-    def candy(self, ratings: List[int]) -> int:
+    def candy(self, ratings: list[int]) -> int:
         """
         Greedily assign lowest candies possible to children with lowest rating.
         O(n) / O(n)     time/space complexity
         """
 
         # store (incremented) indexes of each rating
-        rating_index_map = defaultdict(list)
+        rating_index_map: defaultdict[int, list[int]] = defaultdict(list)
         for i, rating in enumerate(ratings, start=1):
             rating_index_map[rating].append(i)
 
@@ -29,14 +30,34 @@ class Solution:
 
         # go through ratings in increasing order
         for rating, idxs in sorted(rating_index_map.items()):
-            # iterating through ratings in ascending order means that candies required for at i is
-            # max(neighbors_candies) + 1. Have to store in temporary array, because neighboring
-            # indexes with the same rating can have the same amount of candy
-            to_insert = [max(candies[idx-1], candies[idx+1]) + 1 for idx in idxs]
+            # iterating through ratings in ascending order means that candies
+            # required for at i is max(neighbors_candies) + 1. The candy
+            # assignments have to be stored in a temporary array, because
+            # neighboring indexes with the same rating can have the same
+            # amount of candy
+            to_insert = [max(candies[idx - 1], candies[idx + 1]) + 1 for idx in idxs]
 
             # update candies
-            for j, idx in enumerate(idxs):
-                candies[idx] = to_insert[j]
+            for c, idx in enumerate(idxs):
+                candies[idx] = to_insert[c]
 
         # return sum of candies needed
+        return sum(candies)
+
+    def candy_two_pass(self, ratings: list[int]) -> int:
+        n = len(ratings)
+        candies = [1] * n
+
+        # update from left to right, incrementing candies
+        # if rating higher than previous
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # update from right to left, incrementing if rating higher than
+        # next and current candies lower than next
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
+                candies[i] = candies[i + 1] + 1
+
         return sum(candies)
