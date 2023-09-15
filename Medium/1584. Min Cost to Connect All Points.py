ecce0d1ca1d41a4f0ae1f8f09052d6f7015c391c
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-15 06:59:30 +0200

    [1584.] Updated

--- Medium/1584. Min Cost to Connect All Points.py
+++ Medium/1584. Min Cost to Connect All Points.py
@@ -1,68 +1,50 @@
 import heapq
-from typing import List
 
 
 class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+    """
+    You are given an array points representing integer coordinates of some points
+    on a 2D-plane, where points[i] = [xi, yi].
+
+    The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance
+    between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
+
+    Return the minimum cost to make all points connected. All points are connected
+    if there is exactly one simple path between any two points.
+
+    Constraints:
+    - 1 <= points.length <= 1000
+    - -10^6 <= xi, yi <= 10^6
+    - All pairs (xi, yi) are distinct.
+    """
+
+    def minCostConnectPoints(self, points: list[list[int]]) -> int:
+        """
+        Finds an MST using Prims algorithm.
+        O(n^2) / O(n)   time / space complexity
+        """
+        max_int = 2 << 30
+        # graph[i] = [shortest distance to exising MST, x, y]
+        points_with_dist = [[max_int, x, y] for (x, y) in points]
+        # start with "first" point, set its distance to 0 to use it as a starting node
+        points_with_dist[0][0] = 0
         # result variable
         cost = 0
-
-        # constraint: -10^6 <= xi, yi <= 10^6
-        max_int = 2 << 30
-
-        n = len(points)
-
-        # smallest distance to any previous point
-        shortest_dist = [max_int for _ in range(n)]
-
-        # vertex 0 will virtually connect to nothing at first iteration of prims algorithm
-        shortest_dist[0] = 0
-
-        # construct edges between all vertices in O(n^2) time and space
-        edges = [[0]*n for _ in range(n)]
-        for i, (x1, y1) in enumerate(points):
-            for j, (x2, y2) in enumerate(points[i + 1:], start=i + 1):
-                distance = abs(x1 - x2) + abs(y1 - y2)
-                edges[i][j] = distance
-                edges[j][i] = distance
-
-        # construct a set tracking remaining nodes in O(n) time and space
-        remaining = set(i for i in range(n))
-
-        # use version of prims algorithm with n iterations
-        while remaining:
-            # find the vertex with the smallest distance to any previous vertex in O(n) time
-            v = -1
-            smallest_edge = max_int + 1
-            for i in remaining:
-                if shortest_dist[i] < smallest_edge:
-                    v = i
-                    smallest_edge = shortest_dist[i]
-
-            remaining.remove(v)
-            cost += smallest_edge
-
-            # update shortest dist of all remaining nodes in O(n) time
-            vw_distances = edges[v]
-            for w in remaining:
-                distance = vw_distances[w]
-                if (distance < shortest_dist[w]):
-                    shortest_dist[w] = distance
-
-        return cost
-
-    def minCostConnectPointsStolen(self, points: List[List[int]]) -> int:
-        max_int = 2 << 30
-        graph = [[max_int, x, y] for (x, y) in points]
-        graph[0][0] = 0
-        cost = 0
-        while graph:
-            dist, x, y = heapq.heappop(graph)
+        # while points left to add to the graph
+        while points_with_dist:
+            # get vertex closest to existing MST and pop from heap
+            dist, x, y = heapq.heappop(points_with_dist)
+            # add its distance to the total cost
             cost += dist
-            for neighbor in graph:
+            # update shortest distance to updated MST
+            for neighbor in points_with_dist:
                 old_dist, neighbor_x, neighbor_y = neighbor
-                dist_to_curr = abs(x-neighbor_x) + abs(y-neighbor_y)
+                # calculate distance to current node
+                dist_to_curr = abs(x - neighbor_x) + abs(y - neighbor_y)
                 if dist_to_curr < old_dist:
+                    # update shortest distance if smaller
                     neighbor[0] = dist_to_curr
-            heapq.heapify(graph)
+            # heapify to make sure node with shortest distance is at the top of the heap
+            heapq.heapify(points_with_dist)
+
         return cost
