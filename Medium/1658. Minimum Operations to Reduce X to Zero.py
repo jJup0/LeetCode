f576034d7d9c0c817051f1a82b70a2a4db0d96c8
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-22 05:24:53 +0200

    [1658.] Updated

--- Medium/1658. Minimum Operations to Reduce X to Zero.py
+++ Medium/1658. Minimum Operations to Reduce X to Zero.py
@@ -1,25 +1,25 @@
-from typing import List
-
-
 class Solution:
     """
-    You are given an integer array nums and an integer x. In one operation, you can either
-    remove the leftmost or the rightmost element from the array nums and subtract its value
-    from x. Note that this modifies the array for future operations.
+    You are given an integer array nums and an integer x. In one operation, you
+    can either remove the leftmost or the rightmost element from the array nums
+    and subtract its value from x. Note that this modifies the array for future
+    operations.
+
     Return the minimum number of operations to reduce x to exactly 0 if it is possible,
     otherwise, return -1.
+
     Constraints:
-        1 <= nums.length <= 10^5
-        1 <= nums[i] <= 10^4
-        1 <= x <= 10^9
+    - 1 <= nums.length <= 10^5
+    - 1 <= nums[i] <= 10^4
+    - 1 <= x <= 10^9
     """
 
-    def minOperations(self, nums: List[int], x: int) -> int:
-
+    def minOperations(self, nums: list[int], x: int) -> int:
         n = len(nums)
 
         # default result value if no operation are found
-        res = n + 1
+        INF = n + 1
+        res = INF
 
         # start by calculating index i for which sum[:i] > x
         i = running_sum = 0
@@ -29,18 +29,20 @@ class Solution:
                 break
 
         # if the total sum of nums is smaller than x return -1
-        if (running_sum < x):
+        if running_sum < x:
             return -1
 
-        # reduce running sum by last element that made it greater than total
-        running_sum -= nums[i]
-        i -= 1
+        # j index from which to currently take values from the right side
+        j = len(nums) - 1
+        # iterate i down to 0
+        for i in range(i, -1, -1):
+            # assert running_sum >= x
 
-        # j is end index (taking values from right until n - j)
-        j = -1
-        for i in range(i, -2, -1):
+            # take one less item from left side
+            running_sum -= nums[i]
+
             # if current amount of nums to take from right is greater than res, break
-            if -j > res:
+            if n - 1 - j > res:
                 break
 
             # keep increasing amount of nums to take from the array while less than target
@@ -50,14 +52,11 @@ class Solution:
 
             # if target is reached, update res if current items taken is less than res
             if running_sum == x:
-                items_taken = i - j
-                if (items_taken < res):
+                items_taken = i + (n - 1 - j)
+                if items_taken < res:
                     res = items_taken
 
-            # take one less item from left side and repeat
-            running_sum -= nums[i]
-
         # if default value still in res, return -1
-        if res == n + 1:
+        if res == INF:
             return -1
         return res
