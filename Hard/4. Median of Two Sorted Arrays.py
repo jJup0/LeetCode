5aea8173617c2b1d7258e43ab0a4c083b94d15ae
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-22 05:40:38 +0200

    [4.] Solved

--- Hard/4. Median of Two Sorted Arrays.py
+++ Hard/4. Median of Two Sorted Arrays.py
@@ -1,50 +1,55 @@
-from typing import List
-import statistics
-import random
-
-
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) < len(nums2):
+    """
+    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+    The overall run time complexity should be O(log (m+n)).
+
+    Constraints:
+    - nums1.length == m
+    - nums2.length == n
+    - 0 <= m <= 1000
+    - 0 <= n <= 1000
+    - 1 <= m + n <= 2000
+    - -10^6 <= nums1[i], nums2[i] <= 10^6
+    """
+
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
-        n, m = len(nums1), len(nums2)
-        half_len = (n + m) >> 1
-        lo1, hi1 = half_len - m + 1, half_len - 1
+        m = len(nums1)
+        n = len(nums2)
+        low_1 = 0
+        high_1 = m
+        while low_1 <= high_1:
+            # get middle index for nums1 and nums2
+            mid_1 = (low_1 + high_1) // 2
+            mid_2 = (m + n + 1) // 2 - mid_1
 
-        while lo1 < hi1:
-            mid1 = (lo1 + hi1) >> 1
-            mid2 = half_len - mid1
-            try:
-                v1, v2 = nums1[mid1], nums2[mid2]
-            except:
-                print(f"\n\nm1, m2 - l1, l2: {(mid1, mid2)}{(n, m)}\n")
-                return -1
-            if v1 <= v2:
-                lo1 = mid1 + 1
-            else:
-                hi1 = mid1 - 1
+            # from these indexes get lower and upper median candidates for each array
+            # if an index is out of bounds, use inf or -inf
+            upper_1 = float("inf") if mid_1 == m else nums1[mid_1]
+            lower_1 = float("-inf") if mid_1 == 0 else nums1[mid_1 - 1]
+            upper_2 = float("inf") if mid_2 == n else nums2[mid_2]
+            lower_2 = float("-inf") if mid_2 == 0 else nums2[mid_2 - 1]
 
-        mid1 = (lo1 + hi1) >> 1
-        mid2 = half_len - mid1
-        try:
-            return (nums1[mid1] + nums2[mid2]) / 2
-        except:
-            print(f"\n\nm1, m2 - l1, l2: {(mid1, mid2)}{(n, m)}\n")
-            return -1
+            if lower_1 <= upper_2 and lower_2 <= upper_1:
+                # current "lower" median candidates are both smaller equal to
+                # the "upper" median candidate of the other array
+                if (m + n) % 2 == 0:
+                    # even total length, take average of two middle values
+                    # middle values are largest lower candidate and smallest upper candidate
+                    return (max(lower_1, lower_2) + min(upper_1, upper_2)) / 2
+                else:
+                    # odd length, take larger of the two values
+                    return max(lower_1, lower_2)
+            elif lower_1 > upper_2:
+                # lower median candidate of first array is larger than
+                # upper median candidate of second array, index mid_1
+                # (and so also high_1) is too large to be a median candidate
+                high_1 = mid_1 - 1
+            else:  # lower_2 > upper_1
+                # analogous to case above
+                low_1 = mid_1 + 1
 
-
-# S = Solution()
-
-# # random.seed(1)
-# nsize = 20000
-# for _ in range(100):
-#     s1, s2 = tuple(random.randint(1, 1000) for _ in range(2))
-
-#     n1, n2 = tuple(sorted(random.randint(1, nsize) for _ in range(s1)) for _ in range(2))
-
-#     total = sorted(n1 + n2)
-
-#     x = S.findMedianSortedArrays(n1, n2)
-#     real = statistics.median(total)
-#     print(abs(x - real), end = "\t")
+        assert False
