3b580a221a5a1c94d3abe75f0c8582a7a10bad09
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-30 08:12:35 +0200

    [456.] Updated

--- Medium/456. 132 Pattern.py
+++ Medium/456. 132 Pattern.py
@@ -3,27 +3,28 @@ from typing import List
 
 class Solution:
     """
-    Given an array of n integers nums, a 132 pattern is a subsequence of three integers
-    nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+    Given an array of n integers nums, a 132 pattern is a subsequence of three
+    integers nums[i], nums[j] and nums[k] such that i < j < k and
+    nums[i] < nums[k] < nums[j].
+
     Return true if there is a 132 pattern in nums, otherwise, return false.
+
     Constraints:
-        n == nums.length
-        1 <= n <= 2 * 10^5
-        -10^9 <= nums[i] <= 10^9
+    - n == nums.length
+    - 1 <= n <= 2 * 10^5
+    - -10^9 <= nums[i] <= 10^9
     """
 
     def find132pattern(self, nums: List[int]) -> bool:
-
         # go through list in reverse order, keep stack of all values that have
         # been bigger than biggest value so far, as potential future candidates
         # for being nums[k] in the 132 pattern. top of stack is lowest number,
         # increasing from there.
-        stack = []
+        stack: list[int] = []
 
         # value to store hightest nums[k] possible (second highest number while going in reverse order)
-        nums_k = float('-inf')
+        nums_k = float("-inf")
         for num in reversed(nums):
-
             # if num is smaller than nums_k, num is nums[i] candidate and return True
             if num < nums_k:
                 return True
@@ -33,7 +34,8 @@ class Solution:
             while stack and stack[-1] < num:
                 nums_k = stack.pop()
 
-            # append num to stack, as it is potential future nums[k] candidate
+            # append num (current nums[j] candidate) to stack,
+            # it is potential future nums[k] candidate
             stack.append(num)
 
         return False
