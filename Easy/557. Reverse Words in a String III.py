2c9f1c6c3b3d03ec01c911ac7e1bcdbe04b0642e
Jakob Roithinger <jake.roithinger@gmail.com>
2023-10-01 07:05:36 +0200

    [557.] Updated

--- Easy/557. Reverse Words in a String III.py
+++ Easy/557. Reverse Words in a String III.py
@@ -1,13 +1,14 @@
 class Solution:
     """
-    Given a string s, reverse the order of characters in each word within a sentence while still
-    preserving whitespace and initial word order.
+    Given a string s, reverse the order of characters in each word within a
+    sentence while still preserving whitespace and initial word order.
+
     Constraints:
-        1 <= s.length <= 5 * 10^4
-        s contains printable ASCII characters.
-        s does not contain any leading or trailing spaces.
-        There is at least one word in s.
-        All the words in s are separated by a single space.
+    - 1 <= s.length <= 5 * 10^4
+    - s contains printable ASCII characters.
+    - s does not contain any leading or trailing spaces.
+    - There is at least one word in s.
+    - All the words in s are separated by a single space.
     """
 
     def reverseWords(self, s: str) -> str:
@@ -15,4 +16,4 @@ class Solution:
         O(n) / O(n)     time / space complexity
         """
         # split, reverse and rejoin
-        return ' '.join(word[::-1] for word in s.split())
+        return " ".join(word[::-1] for word in s.split())
