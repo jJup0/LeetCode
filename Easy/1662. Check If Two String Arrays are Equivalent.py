b1878e1b4c791f70dd7e970ff508ec65bf941e7f
Jakob Roithinger <jake.roithinger@gmail.com>
2023-12-02 08:44:27 +0100

    [1662.] Solved

--- Easy/1662. Check If Two String Arrays are Equivalent.py
+++ Easy/1662. Check If Two String Arrays are Equivalent.py
@@ -1,8 +1,20 @@
-from typing import List
+"""
+Given two string arrays word1 and word2, return true if the two arrays
+represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated
+in order forms the string.
+
+Constraints:
+- 1 <= word1.length, word2.length <= 10^3
+- 1 <= word1[i].length, word2[i].length <= 10^3
+- 1 <= sum(word1[i].length), sum(word2[i].length) <= 10^3
+- word1[i] and word2[i] consist of lowercase letters.
+"""
 
 
 class Solution:
-    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+    def arrayStringsAreEqual(self, word1: list[str], word2: list[str]) -> bool:
         """
         O(min(n, m)) / O(1)     time / space complexity
         """
@@ -14,13 +26,15 @@ class Solution:
         # extra letter, zip will call next() on both gen1 and gen2, consuming the next letter of
         # gen1, but will break on the StopIteration of gen2, so when compared for exhaustion true
         # is returned. Example: word1 = ["ab"], word2 = ["a"]
-        string2_starts_with_string1 = all(c1 == next(gen_string2, None) for c1 in gen_string1)
+        string2_starts_with_string1 = all(
+            c1 == next(gen_string2, None) for c1 in gen_string1
+        )
 
         # check if gen2 is also exhausted
         return string2_starts_with_string1 and (next(gen_string2, None) == None)
 
-    def arrayStringsAreEqual_oneline(self, word1: List[str], word2: List[str]) -> bool:
+    def arrayStringsAreEqual_oneline(self, word1: list[str], word2: list[str]) -> bool:
         """
         O(n + m) / O(n + m)     time / space complexity
         """
-        return ''.join(word1) == ''.join(word2)
+        return "".join(word1) == "".join(word2)
