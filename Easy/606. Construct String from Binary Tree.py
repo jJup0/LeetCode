ec8994b50b8910e7dc26bf2053e115eedadc7e7b
Jakob Roithinger <jake.roithinger@gmail.com>
2023-12-08 07:57:41 +0100

    [606.] Updated

--- Easy/606. Construct String from Binary Tree.py
+++ Easy/606. Construct String from Binary Tree.py
@@ -1,36 +1,44 @@
-from typing import Optional
+"""
+Given the root of a binary tree, construct a string consisting of
+parenthesis and integers from a binary tree with the preorder traversal
+way, and return it.
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+Omit all the empty parenthesis pairs that do not affect the one-to-one
+mapping relationship between the string and the original binary tree.
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 10^4].
+- -1000 <= Node.val <= 1000
+"""
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    # Definition for a binary tree node.
+    class TreeNode:
+        def __init__(
+            self,
+            val: int = 0,
+            left: "TreeNode" | None = None,
+            right: "TreeNode" | None = None,
+        ):
+            self.val = val
+            self.left = left
+            self.right = right
 
 
 class Solution:
-    """
-    Given the root of a binary tree, construct a string consisting of parenthesis and integers
-    from a binary tree with the preorder traversal way, and return it.
-    Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship
-    between the string and the original binary tree.
-    Constraints:
-        The number of nodes in the tree is in the range [1, 10^4].
-        -1000 <= Node.val <= 1000
-    """
-
     def tree2str(self, root: Optional[TreeNode]) -> str:
         if not root:
             return ""
         # get string representations of child nodes
         left_str = self.tree2str(root.left)
         right_str = self.tree2str(root.right)
-        # if right child is not empty include both
         if right_str:
+            # if right child is not empty include both
             return f"{root.val}({left_str})({right_str})"
-        # if only left child is non-empty, do not include right
         elif left_str:
+            # if only left child is non-empty, do not include right
             return f"{root.val}({left_str})"
-        # if node is leaf, return value as string
         else:
+            # if node is leaf, return value as string
             return str(root.val)
