d567833fb1be263372bcd428388c6f7329a9ea03
Jakob Roithinger <jake.roithinger@gmail.com>
2023-12-10 11:05:33 +0100

    [867.] Solved

--- Easy/867. Transpose Matrix.py
+++ Easy/867. Transpose Matrix.py
@@ -1,19 +1,32 @@
-from typing import List
+"""
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main
+diagonal, switching the matrix's row and column indices.
+
+Constraints:
+- m == matrix.length
+- n == matrix[i].length
+- 1 <= m, n <= 1000
+- 1 <= m * n <= 105
+- -10^9 <= matrix[i][j] <= 10^9
+"""
 
 
 class Solution:
-    """
-    Given a 2D integer array matrix, return the transpose of matrix.
-    The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
-    Constraints:
-        m == matrix.length
-        n == matrix[i].length
-        1 <= m, n <= 1000
-        1 <= m * n <= 10^5
-        -10^9 <= matrix[i][j] <= 10^9
-    """
+    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+        return self.transpose_list_comprehension(matrix)
 
-    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        # construct a single row by iterating over rows in inner loop, and columns in outer loop
-        return [[matrix[i][j] for i in range(len(matrix))]
-                for j in range(len(matrix[0]))]
+    def transpose_regular(self, matrix: list[list[int]]) -> list[list[int]]:
+        height = len(matrix)
+        width = len(matrix[0])
+        new_matrix = [[0] * height for _ in range(width)]
+        for i, row in enumerate(matrix):
+            for j, val in enumerate(row):
+                new_matrix[j][i] = val
+        return new_matrix
+
+    def transpose_list_comprehension(self, matrix: list[list[int]]) -> list[list[int]]:
+        height = len(matrix)
+        width = len(matrix[0])
+        return [[matrix[i][j] for i in range(height)] for j in range(width)]
