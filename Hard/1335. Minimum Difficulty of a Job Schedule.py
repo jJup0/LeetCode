478d4f57f4b2891c51c59293313a37be7fb4f29e
Jakob Roithinger <jake.roithinger@gmail.com>
2023-12-30 10:30:27 +0100

    [1335.] Solved

--- Hard/1335. Minimum Difficulty of a Job Schedule.py
+++ Hard/1335. Minimum Difficulty of a Job Schedule.py
@@ -1,126 +1,126 @@
 import heapq
-from typing import List, Tuple
 
 
 class Solution:
-    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+    def minDifficulty(self, job_difficulty: list[int], d: int) -> int:
+        return self.minDifficulty_fast(job_difficulty, d)
+
+    def minDifficulty_fast(self, job_difficulty: list[int], d: int) -> int:
+        """
+        n := len(job_difficulty)
+        O(d * n) / O(n)     time / space complexity
+        """
         INT_INF = 1_000_000
 
-        n = len(jobDifficulty)
+        n = len(job_difficulty)
         if n < d:
             return -1
 
-        # dp[i] is difficulty to complete up to ith job with k days, dp2 is with k+1 days
-        dp = [INT_INF] * n
-        dp2 = [0] * n
-        for day in range(d):
-            stack = []
-            for i in range(day, n):
-                
-                # complete ith job as the "only" job on a day
-                dp2[i] = dp[i - 1] + jobDifficulty[i] if i else jobDifficulty[i]
-                
-                # while the top top on the stack is less/equal to the current job, replace 
-                while stack and jobDifficulty[stack[-1]] <= jobDifficulty[i]:
-                    j = stack.pop()
-                    dp2[i] = min(dp2[i], dp2[j] - jobDifficulty[j] + jobDifficulty[i])
-                
-                if stack:
-                    dp2[i] = min(dp2[i], dp2[stack[-1]])
+        self._job_difficulty = job_difficulty
 
-                stack.append(i)
+        # prev_dp[i] is difficulty to complete up to ith job with `current_day` days,
+        # next_dp[i] is with `current_day + 1` days
+        prev_dp = [INT_INF] * n
+        next_dp = [0] * n
+        for current_day in range(d):
+            # stack of jobs where for job_idx_stack[i], job_idx_stack[j]:
+            # `job_difficulty[i] > job_difficulty[j]` if `i < j`
+            job_idx_stack: list[int] = []
+            # first job that can be taken is at `current_day`th job,
+            # since at least one job a day must be completed
+            for job_idx in range(current_day, n):
+                next_dp[job_idx] = self._calc_min_diff(
+                    prev_dp, next_dp, job_idx_stack, job_idx
+                )
 
-            dp, dp2 = dp2, dp
+            # switch list pointers, data now in next_dp is invalid
+            prev_dp, next_dp = next_dp, prev_dp
 
-        return dp[-1]
+        return prev_dp[-1]
 
+    def _calc_min_diff(
+        self,
+        prev_dp: list[int],
+        next_dp: list[int],
+        job_idx_stack: list[int],
+        starting_job_idx: int,
+    ) -> int:
+        current_job_diff = self._job_difficulty[starting_job_idx]
+        # base difficulty is difficulty to only complete current job
+        min_diff_upto_job = (
+            prev_dp[starting_job_idx - 1] + current_job_diff
+            if starting_job_idx
+            else current_job_diff
+        )
 
-    def minDifficulty_partition_flawed_logic(self, jobDifficulty: List[int], d: int) -> int:
-        MAX_IMPOSSIBLE = 1_000_000
+        # while the top of the stack is less/equal to the current job,
+        # include the easier job in the current day
+        while (
+            job_idx_stack
+            and self._job_difficulty[job_idx_stack[-1]] <= current_job_diff
+        ):
+            job_2_idx = job_idx_stack.pop()
+            job_d_diff = self._job_difficulty[job_2_idx]
+            assert job_2_idx < starting_job_idx
+            assert job_d_diff < current_job_diff
+            # update minimum difficulty if it is easier 
+            min_diff_upto_job = min(
+                min_diff_upto_job,
+                next_dp[job_2_idx] - self._job_difficulty[job_2_idx] + current_job_diff,
+            )
 
-        def partition(l: int, r: int) -> Tuple[int, int, int, int, int, int]:
-            if r-l == 1:
-                return MAX_IMPOSSIBLE, -1, -1, -1, l, r
+        if job_idx_stack:
+            # if total difficulty for a more difficult job is less than the
+            # current minimum difficulty, update minimum difficulty
+            min_diff_upto_job = min(min_diff_upto_job, next_dp[job_idx_stack[-1]])
 
-            max_left = [0] * (r-l)
-            max_right = [0] * (r-l)
+        job_idx_stack.append(starting_job_idx)
+        return min_diff_upto_job
 
-            m = 0
-            for i in range(l, r):
-                m = max(m, jobDifficulty[i])
-                max_left[i-l-1] = m
-            max_left[0] = MAX_IMPOSSIBLE
-            
+    def minDifficulty_heap(self, job_difficulty: list[int], d: int) -> int:
+        """
+        n := len(job_difficulty)
+        O(n^2 * d) / O(n^2 * d)     time / space complexity
+        """
+        n = len(job_difficulty)
+        if n < d:
+            return -1
 
-            m = 0
-            for i in range(r-1, l, -1):
-                m = max(m, jobDifficulty[i])
-                max_right[i-l] = m
+        INT_INF = 1_000_000
 
-            m = MAX_IMPOSSIBLE
-            m_i = -1
-            for i, (ml, mr) in enumerate(zip(max_left, max_right)):
-                m_curr = ml + mr
-                if m_curr < m:
-                    m = m_curr
-                    m_i = i
-            return m, max_left[m_i], max_right[m_i], m_i, l, r
+        # dp[i][j] = minimum difficulty for completing `j` jobs in `i` days
+        dp = [[INT_INF] * (n + 1) for _ in range(d + 1)]
 
-        ls = {(0, len(jobDifficulty)): max(jobDifficulty)}
-        for _ in range(d-1):
-            partition_results = []
-            for l, r in ls:
-                partition_results.append(partition(l, r))
+        # heap[i] = total_difficulty, -day, -job_index. Values are negated
+        # to prefer later days and more jobs completed
+        heap: list[tuple[int, int, int]] = [(0, 0, 0)]
+        while heap and dp[-1][-1] == INT_INF:
+            difficulty, neg_day, neg_job_i = heapq.heappop(heap)
+            day = -neg_day
+            job_i = -neg_job_i
 
-            _, diff_l, diff_r, split_idx, l, r = min(partition_results)
-            ls.pop((l, r))
-            ls[(l, split_idx)] = diff_l
-            ls[(split_idx, r)] = diff_r
-
-        print(sorted(ls.items()))
-
-        return sum(ls.values())
-
-    def minDifficulty_heap(self, jobDifficulty: List[int], d: int) -> int:
-
-        MAX_IMPOSSIBLE = 1_000_000
-        n = len(jobDifficulty)
-
-        dp = [[MAX_IMPOSSIBLE] * (n + 1) for _ in range(d + 1)]
-
-        heap = [(0, 0, 0)]
-        while heap and dp[-1][-1] == MAX_IMPOSSIBLE:
-            prev_difficulty, day, job_i = heapq.heappop(heap)
-            day = -day
-            job_i = -job_i
-
-            if prev_difficulty > dp[day][job_i]:
+            # check if lower difficulty has been achieved for this day and job count,
+            # continue if greater equal, else update
+            if difficulty >= dp[day][job_i]:
                 continue
-            dp[day][job_i] = prev_difficulty
+            dp[day][job_i] = difficulty
 
+            # if all days used up, continue
             if day == d:
                 continue
 
             difficulty_today = 0
             for j in range(job_i, n - (d - day) + 1):
-                job = jobDifficulty[j]
+                job = job_difficulty[j]
                 if job > difficulty_today:
                     difficulty_today = job
 
-                heapq.heappush(heap, (prev_difficulty + difficulty_today, -(day + 1), -(j + 1)))
+                heapq.heappush(
+                    heap, (difficulty + difficulty_today, -(day + 1), -(j + 1))
+                )
 
-        res = dp[-1][-1]
-        return -1 if res == MAX_IMPOSSIBLE else res
+        return dp[-1][-1]
 
 
-S = Solution()
-# print(S.minDifficulty(
-#     [6, 5, 4, 3, 2, 1],
-#     2
-# )
-# )
-print(S.minDifficulty(
-    [42, 35, 957, 671, 87, 222, 524, 5, 280, 878, 242, 398, 610, 984, 649, 513, 563, 997, 786, 223, 413, 961, 208, 189, 519, 606, 504, 406, 994, 475, 940, 476, 762, 283, 218, 404, 715, 755, 689, 457, 20, 403, 749, 68, 17, 763, 78, 695, 445, 338, 643, 400, 615, 29, 866, 861, 103, 665, 743, 361, 798, 236, 255, 15, 326, 124, 14, 588, 711, 992, 501, 731, 538, 619, 765, 8, 477, 854, 243, 95, 627, 480, 505, 800, 818, 722, 194, 717, 305, 810, 497, 686, 704, 322, 532, 22, 234, 290, 885, 416, 155, 428, 161, 315, 152, 441, 730, 926, 175, 536, 646, 922, 951, 101, 107, 233, 61, 735, 669, 512, 28, 569, 447, 982, 916, 321, 1000, 754, 483, 482, 811, 654, 47, 344, 772, 978, 467, 308, 472, 269, 0, 252, 131, 834, 303, 945, 469, 785, 537, 188, 449,
-        675, 528, 733, 271, 541, 822, 328, 904, 876, 889, 55, 16, 853, 154, 767, 573, 925, 279, 697, 525, 431, 375, 958, 836, 911, 166, 965, 523, 709, 923, 587, 603, 226, 354, 641, 620, 316, 110, 292, 529, 943, 1, 151, 737, 959, 27, 56, 353, 681, 26, 677, 337, 723, 12, 914, 955, 134, 370, 260, 490, 684, 364, 618, 232, 870, 985, 196, 225, 359, 129, 58, 341, 67, 494, 757, 229, 323, 256, 21, 783, 692, 642, 37, 909, 248, 81, 345, 425, 478, 651, 309, 435, 10, 534, 450, 576, 144, 201, 496, 267, 609, 11, 454, 531, 966, 156, 176, 190, 542, 856, 365, 983, 947, 758, 950, 388, 820, 867, 833, 605, 741, 34, 663, 884, 65, 628, 969, 864, 664, 718, 805, 891, 657, 863, 960, 518, 71, 300, 756, 613, 667, 228, 274, 971, 970, 552, 556, 648, 251], 10
-)
-)
+s = Solution()
+print(s.minDifficulty_fast([6, 5, 4, 3, 2, 1], 2))
