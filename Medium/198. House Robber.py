b6b8e53d198bccc7bcc686079171477449181cb8
Jakob Roithinger <jake.roithinger@gmail.com>
2024-01-21 11:53:59 +0100

    [198.] Updated

--- Medium/198. House Robber.py
+++ Medium/198. House Robber.py
@@ -1,7 +1,31 @@
+"""
+You are a professional robber planning to rob houses along a street. Each house
+has a certain amount of money stashed, the only constraint stopping you from
+robbing each of them is that adjacent houses have security systems connected
+and it will automatically contact the police if two adjacent houses were broken
+into on the same night.
+
+Given an integer array nums representing the amount of money of each house,
+return the maximum amount of money you can rob tonight without alerting the
+police.
+
+Constraints:
+- 1 <= nums.length <= 100
+- 0 <= nums[i] <= 400
+"""
+
+
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        dp1, dp2 = 0, 0
+    def rob(self, nums: list[int]) -> int:
+        """
+        O(n) / O(1)     time / space complexity
+        """
+        max_rob = 0
+        no_rob_prev = 0
         for num in nums:
-            dp1, dp2 = dp2, max(dp1 + num, dp2)
-        return dp2 
-    
+            prev_max_rob = max_rob
+            # maximum profit is largest between max profit of previous house,
+            # and robbing this house and not robbing the previous house
+            max_rob = max(no_rob_prev + num, max_rob)
+            no_rob_prev = prev_max_rob
+        return max_rob
