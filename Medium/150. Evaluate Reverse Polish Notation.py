e29340d857d0156cd79d3b6eb838c33dc0425fa9
Jakob Roithinger <jake.roithinger@gmail.com>
2024-01-30 20:19:50 +0100

    [150.] Updated

--- Medium/150. Evaluate Reverse Polish Notation.py
+++ Medium/150. Evaluate Reverse Polish Notation.py
@@ -1,23 +1,33 @@
-from typing import List
+"""
+You are given an array of strings tokens that represents an arithmetic
+expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the
+expression.
+
+Note that:
+- The valid operators are '+', '-', '*', and '/'.
+- Each operand may be an integer or another expression.
+- The division between two integers always truncates toward zero.
+- There will not be any division by zero.
+- The input represents a valid arithmetic expression in a reverse polish notation.
+- The answer and all the intermediate calculations can be represented in a
+  32-bit integer.
+
+Constraints:
+- 1 <= tokens.length <= 10^4
+- tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the
+  range [-200, 200].
+"""
 
 
 class Solution:
-    """
-    Evaluate the value of an arithmetic expression in Reverse Polish Notation.
-    Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
-    Note that division between two integers should truncate toward zero.
-    It is guaranteed that the given RPN expression is always valid. That means the
-    expression would always evaluate to a result, and there will not be any division by zero operation.
-    Constraints:
-        1 <= tokens.length <= 10^4
-        tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
-    """
-
-    def evalRPN(self, tokens: List[str]) -> int:
-        # uses Python 3.10 match syntax
-
+    def evalRPN(self, tokens: list[str]) -> int:
+        """
+        O(n) / O(n)     time / space complexity
+        """
         # operands stack
-        stack = []
+        stack: list[int] = []
         for token in tokens:
             # any binary operation works on the top two items in the stack, i.e
             # consumes the top operand and applies it to the new top operand
@@ -38,6 +48,6 @@ class Solution:
                     # if the token is not an operation, it is an integer, so append it to the operands stack
                     stack.append(int(token))
 
-        # if input is correct, one "operand" will remain
+        # if input is correct, one operand will remain
         assert len(stack) == 1
         return stack[0]
