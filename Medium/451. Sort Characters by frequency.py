9d3b0c15c668941aa794bebaae6009e5d2c59540
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-10 09:40:17 +0100

    [451.] Updated

--- Medium/451. Sort Characters by frequency.py
+++ Medium/451. Sort Characters by frequency.py
@@ -1,11 +1,19 @@
+"""
+Given a string s, sort it in decreasing order based on the frequency of the characters.
+The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+Constraints:
+- 1 <= s.length <= 5 * 10^5
+- s consists of uppercase and lowercase English letters and digits.
+"""
+
 from collections import Counter
 
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-
-        l = Counter(s)
-        retVal = ''
-        for char, count in (sorted(l.items(), key=lambda item: item[1], reverse=True)):
-            retVal += char*count
-        return retVal
+        cs = Counter(s)
+        elems = sorted(cs.items(), key=lambda char_count: char_count[1], reverse=True)
+        return "".join(c * count for count, c in elems)
