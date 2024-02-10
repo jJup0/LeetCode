0661295615ad4f002e358968872939cd76ef8e01
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-10 09:16:42 +0100

    [647.] Updated

--- Medium/647. Palindromic Substrings.py
+++ Medium/647. Palindromic Substrings.py
@@ -1,46 +1,64 @@
+"""
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+Constraints:
+- 1 <= s.length <= 1000
+- s consists of lowercase English letters.
+"""
+
+
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        return self.countSubstrings_v2(s)
+
+    def countSubstrings_v2(self, s: str) -> int:
+        """
+        O(n) / O(n)     time / space complexity
+        """
+        # buffer s to avoid out of bounds checks, results in O(n) extra space
+        s = "\u0000" + s + "\u0001"
+        end_idx = len(s) - 1
+
         # left and right pointer in string from where to build palindrome
         l = r = 1
-
-        # buffer s to avoid out of bounds checks, results in O(n) extra space
-        s = "\n" + s + "$"
-        end_idx = len(s) - 1 
 
         # result variable
         num_palindromes = 0
 
         while r < end_idx:
+            # Step 1. find identical consecutive characters
             starting_char = s[l]
-            
-            # find identical characters in a row
             while s[r] == starting_char:
                 r += 1
-            # palindromes within this sequence is ((r-l) choose 2)
-            num_palindromes += ((r-l)*(r-l+1))//2
+            # palindromes for repeating identical characters is ((r-l) choose 2)
+            num_palindromes += ((r - l) * (r - l + 1)) // 2
 
-            # start building palidrome with center of identical r-l characters (builds even or odd length
-            # palidromes depending on r-l) j is lower index, k is higher index
-            j = l-1
-            k = r
-
+            # Step 2. use this sequence of identical characters as the center to build palindromes
+            # r points to the first character != s[l]
+            ext_left = l - 1
+            ext_right = r
             # while adding characters on either side still makes substring a palindrome add to
             # result variable and increase range of j and k
-            while s[j] == s[k]:
-                j -= 1
-                k += 1
+            while s[ext_left] == s[ext_right]:
+                num_palindromes += 1
+                ext_left -= 1
+                ext_right += 1
 
-            # number of extra palidromes is number of iterations where s[j] == s[k]
-            num_palindromes += k - r
-            
             # new starting position (l) is first index that was not same character as previous ones
             l = r
 
         return num_palindromes
 
-    def ownSolutionO_n_bufferedStringVariant(self, s: str) -> int:
+    def countSubstrings_v1(self, s: str) -> int:
+        """
+        Has quadratic runtime for strings where are characters are the same.
+        """
         n = len(s)
-        buffered_s = '\n' + s + '$'
+        buffered_s = "\u0000" + s + "\u0001"
         res = 1
         prev = buffered_s[1]
         for seed in range(2, n + 1):
@@ -48,14 +66,14 @@ class Solution:
 
             # odd length palidromes
             diff = 1
-            while buffered_s[seed-diff] == buffered_s[seed+diff]:
+            while buffered_s[seed - diff] == buffered_s[seed + diff]:
                 diff += 1
             res += diff
 
             # even length palidromes
             if c == prev:
                 diff = 1
-                while buffered_s[seed-diff-1] == buffered_s[seed+diff]:
+                while buffered_s[seed - diff - 1] == buffered_s[seed + diff]:
                     diff += 1
                 res += diff
 
