c6718c1c3f57b68e86beb98c844fe5dae7c68002
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-18 08:49:48 +0100

    [1642.] Updated

--- Medium/1642. Furthest Building You Can Reach.py
+++ Medium/1642. Furthest Building You Can Reach.py
@@ -1,36 +1,38 @@
+"""
+You are given an integer array heights representing the heights of buildings,
+some bricks, and some ladders.
+
+You start your journey from building 0 and move to the next building by
+possibly using bricks or ladders.
+
+While moving from building i to building i+1 (0-indexed),
+- If the current building's height is greater than or equal to the next
+building's height, you do not need a ladder or bricks.
+- If the current building's height is less than the next building's height, you
+can either use one ladder or (h[i+1] - h[i])bricks.
+
+Return the furthest building index (0-indexed) you can reach if you use the
+given ladders and bricks optimally.
+
+Constraints:
+- 1 <= heights.length <= 10^5
+- 1 <= heights[i] <= 10^6
+- 0 <= bricks <= 10^9
+- 0 <= ladders <= heights.length
+"""
+
 import heapq
-from typing import List
 
 
 class Solution:
-    """
-    You are given an integer array heights representing the heights of buildings, some bricks, and
-    some ladders.
-    You start your journey from building 0 and move to the next building by possibly using bricks
-    or ladders.
-    While moving from building i to building i+1 (0-indexed),
-        If the current building's height is greater than or equal to the next building's height,
-        you do not need a ladder or bricks.
-        If the current building's height is less than the next building's height, you can either
-        use one ladder or (h[i+1] - h[i]) bricks.
-    Return the furthest building index (0-indexed) you can reach if you use the given ladders and
-    bricks optimally.
-    Constraints:
-        1 <= heights.length <= 10^5
-        1 <= heights[i] <= 10^6
-        0 <= bricks <= 10^9
-        0 <= ladders <= heights.length
-    """
-
-    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+    def furthestBuilding(self, heights: list[int], bricks: int, ladders: int) -> int:
         """
-        Greedily use bricks for all ascends, and replace largest previous ascend "in hindsight"
-        with a ladder when bricks run out. Worst case: bricks=0, ladders=n, each building higher
-        than the next: results in heappush and pop at every iteration.
+        Greedily use bricks for all ascends, and replace largest previous ascend
+        "in hindsight" with a ladder when bricks run out.
         # O(n * log(n)) time, O(n) space
         """
         # maintain a max heap of all previous ascends
-        ascends = []
+        ascends: list[int] = []
 
         # height of previous building
         prev_height = heights[0]
