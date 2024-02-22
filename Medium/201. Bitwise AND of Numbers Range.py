00a9d33ca3ff971b4a09e032a2b4d930c2d2ab08
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-22 18:37:44 +0100

    [201.] Updated

--- Medium/201. Bitwise AND of Numbers Range.py
+++ Medium/201. Bitwise AND of Numbers Range.py
@@ -1,10 +1,26 @@
+"""
+Given two integers left and right that represent the range [left, right],
+return the bitwise AND of all numbers in this range, inclusive.
+
+Constraints:
+- 0 <= left <= right <= 2^31 - 1
+"""
+
+
 class Solution:
-    def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        if n >= (m << 1):
-            return 0
-        ret = m
-        for x in range(m+1, n+1):
-            ret &= x
-            if not ret:
-                return 0
-        return ret
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        """
+        O(log(n)) / O(1)    time / space complexity
+        """
+        shift = 0
+        # find highest set bit where left and right differ
+        # all bits up to and including this bit will be set to 0 by bitwise AND
+        # of the number in the range(left, right) all higher bits are identical
+        # for all numbers in the range
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        # assert left == right
+        # return original left with the first `shift` bits set to 0
+        return left << shift
