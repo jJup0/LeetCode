c3ac9e143040ca79690c6dc9641a6e425179f94a
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-22 18:40:17 +0100

    [997.] Updated

--- Easy/997. Find the Town Judge.py
+++ Easy/997. Find the Town Judge.py
@@ -1,20 +1,27 @@
+"""
+In a town, there are n people labeled from 1 to n. There is a rumor that one of
+these people is secretly the town judge.
+
+If the town judge exists, then:
+
+You are given an array trust where trust[i] = [a_i, b_i] representing that the
+person labeled a_i trusts the person labeled b_i. If a trust relationship does
+not exist in trust array, then such a trust relationship does not exist.
+
+Return the label of the town judge if the town judge exists and can be
+identified, or return -1 otherwise.
+
+Constraints:
+- 1 <= n <= 1000
+- 0 <= trust.length <= 10^4
+- trust[i].length == 2
+- All the pairs of trust are unique.
+- a_i != b_i
+- 1 <= a_i, b_i <= n
+"""
+
+
 class Solution:
-    """
-    In a town, there are n people labeled from 1 to n. There is a rumor that one of these people
-    is secretly the town judge.
-
-    If the town judge exists, then:
-
-    The town judge trusts nobody.
-    Everybody (except for the town judge) trusts the town judge.
-    There is exactly one person that satisfies properties 1 and 2.
-    You are given an array trust where trust[i] = [ai, bi] representing that the person labeled
-    ai trusts the person labeled bi.
-
-    Return the label of the town judge if the town judge exists and can be identified, or return
-    -1 otherwise.
-    """
-
     def findJudge(self, n: int, trust: list[list[int]]) -> int:
         """
         Everybody trusts town judge, town judge trusts nobody =>
@@ -22,14 +29,17 @@ class Solution:
 
         O(len(trust)) / O(n)    time / space complexity
         """
+        if n == 1:
+            # edge case needs to be handled separately
+            return 1
 
-        is_trusted_minus_trusts = [0]*(n+1)
+        is_trusted_minus_trusts = [0] * (n + 1)
         for trusts, isTrusted in trust:
             is_trusted_minus_trusts[trusts] -= 1
             is_trusted_minus_trusts[isTrusted] += 1
 
         # people enumeration start at 1
-        for i in range(1, n+1):
-            if is_trusted_minus_trusts[i] == n-1:
+        for i, trust_diff in enumerate(is_trusted_minus_trusts):
+            if trust_diff == n - 1:
                 return i
         return -1
