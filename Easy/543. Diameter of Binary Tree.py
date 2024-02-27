21fc8ba166a0e3a83f146577e3c74ab948622bdc
Jakob Roithinger <jake.roithinger@gmail.com>
2024-02-27 07:35:26 +0100

    [543.] Updated

--- Easy/543. Diameter of Binary Tree.py
+++ Easy/543. Diameter of Binary Tree.py
@@ -1,24 +1,38 @@
-class TreeNode:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # Definition for a binary tree node.
+    class TreeNode:
+        def __init__(
+            self,
+            val: int = 0,
+            left: "TreeNode | None" = None,
+            right: "TreeNode| None" = None,
+        ):
+            self.val = val
+            self.left = left
+            self.right = right
 
 
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        d = 0
+        """
+        For each subtree, calculate the longest path taken through the subtree root.
+        Return the maximum of all these paths.
+        O(total_nodes) / O(depth of root)     time / space complexity
+        """
 
-        def dfs(node):
-            nonlocal d
+        def get_depth_and_update_diameter(node: TreeNode | None) -> int:
+            nonlocal diameter
             if not node:
                 return 0
 
-            l = dfs(node.left)
-            r = dfs(node.right)
+            l = get_depth_and_update_diameter(node.left)
+            r = get_depth_and_update_diameter(node.right)
 
-            d = max(d, l+r)
-            return max(l, r)+1
+            diameter = max(diameter, l + r)
+            return max(l, r) + 1
 
-        dfs(root)
-        return d
+        diameter = 0
+        get_depth_and_update_diameter(root)
+        return diameter
