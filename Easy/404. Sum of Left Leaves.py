8200ca43eef57485619584182908a1471fab2e7e
Jakob Roithinger <jake.roithinger@gmail.com>
2024-04-14 07:06:14 +0200

    [404.] Updated

--- Easy/404. Sum of Left Leaves.py
+++ Easy/404. Sum of Left Leaves.py
@@ -1,14 +1,50 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child
+of another node.
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 1000].
+- -1000 <= Node.val <= 1000
+"""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # Definition for a binary tree node.
+    class TreeNode:
+        def __init__(
+            self,
+            val: int = 0,
+            left: "TreeNode | None" = None,
+            right: "TreeNode | None" = None,
+        ):
+            self.val = val
+            self.left = left
+            self.right = right
+
+
 class Solution:
-    def sumOfLeftLeaves(self, root: Optional[TreeNode], isLeft=False) -> int:
-        if not root:
-            return 0
-        ret = self.sumOfLeftLeaves(root.left, True) + self.sumOfLeftLeaves(root.right)
-        if (not root.left) and (not root.right) and isLeft:
-            ret += root.val
-        return ret
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        """
+        O(n) / O(depth(root))   time / space complexity
+        """
+        return self._sumOfLeftLeaves(root, False)
+
+    def _sumOfLeftLeaves(self, node: TreeNode, is_left: bool) -> int:
+        res = 0
+        is_leaf = True
+        # add sum of children left leaves
+        if node.left:
+            res += self._sumOfLeftLeaves(node.left, True)
+            is_leaf = False
+        if node.right:
+            res += self._sumOfLeftLeaves(node.right, False)
+            is_leaf = False
+
+        if is_leaf and is_left:
+            # add self if leaf and left node
+            res += node.val
+
+        return res
