fb378e45e8b315c1f954eaac08939da8d88d0e59
Jakob Roithinger <jake.roithinger@gmail.com>
2024-05-04 06:34:00 +0200

    [881.] Updated

--- Medium/881. Boats to Save People.py
+++ Medium/881. Boats to Save People.py
@@ -1,17 +1,18 @@
+"""
+You are given an array people where people[i] is the weight of the ith person,
+and an infinite number of boats where each boat can carry a maximum weight of
+limit. Each boat carries at most two people at the same time, provided the sum
+of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+
+Constraints:
+- 1 <= people.length <= 5 * 10^4
+- 1 <= people[i] <= limit <= 3 * 10^4
+"""
+
+
 class Solution:
-    """
-    You are given an array people where people[i] is the weight of the ith person,
-    and an infinite number of boats where each boat can carry a maximum weight of
-    limit. Each boat carries at most two people at the same time, provided the sum
-    of the weight of those people is at most limit.
-
-    Return the minimum number of boats to carry every given person.
-
-    Constraints:
-        1 <= people.length <= 5 * 10^4
-        1 <= people[i] <= limit <= 3 * 10^4
-    """
-
     def numRescueBoats(self, people: list[int], limit: int) -> int:
         """Greedily match heaviest people with lightest people.
         O(n)/ O(n)      time / space complexity
