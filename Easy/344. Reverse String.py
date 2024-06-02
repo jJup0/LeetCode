4ee2dc581f69c9f422cf1bc1a3010a1d073f3768
Jakob Roithinger <jake.roithinger@gmail.com>
2024-06-02 07:24:32 +0200

    [344.] Solved

--- Easy/344. Reverse String.py
+++ Easy/344. Reverse String.py
@@ -1,10 +1,22 @@
-from typing import List
+"""
+Write a function that reverses a string. The input string is given as an array
+of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Constraints:
+- 1 <= s.length <= 10^5
+- s[i] is a printable ascii character.
+"""
 
 
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        O(n) / O(1)     time / space complexity
+        """
         # s.reverse() in long form:
-        for i in range(len(s)//2):
+        for i in range(len(s) // 2):
             temp = s[i]
             s[i] = s[-(i + 1)]
             s[-(i + 1)] = temp
