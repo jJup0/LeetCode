ea9c1084efd683640691db72ee66b130576a1075
Jakob Roithinger <jake.roithinger@gmail.com>
2024-06-08 20:48:03 +0200

    [523.] Updated

--- Medium/523. Continuous Subarray Sum.py
+++ Medium/523. Continuous Subarray Sum.py
@@ -1,25 +1,30 @@
-from typing import List
+"""
+Given an integer array nums and an integer k, return true if nums has a good
+subarray or false otherwise.
+
+A good subarray is a subarray where:
+- its length is at least two, and
+- the sum of the elements of the subarray is a multiple of k.
+
+Note that:
+- A subarray is a contiguous part of the array.
+- An integer x is a multiple of k if there exists an integer n such that
+  x = n * k. 0 is always a multiple of k.
+
+Constraints:
+- 1 <= nums.length <= 10^5
+- 0 <= nums[i] <= 10^9
+- 0 <= sum(nums[i]) <= 2^31 - 1
+- 1 <= k <= 2^31 - 1
+"""
 
 
 class Solution:
-    """
-    Given an integer array nums and an integer k, return true if nums has a continuous subarray of
-    size at least two whose elements sum up to a multiple of k, or false otherwise.
-
-    An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a
-    multiple of k.
-
-    Constraints:
-        1 <= nums.length <= 10^5
-        0 <= nums[i] <= 10^9
-        0 <= sum(nums[i]) <= 2^31 - 1
-        1 <= k <= 2^31 - 1
-    """
-
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+    def checkSubarraySum(self, nums: list[int], k: int) -> bool:
         """
-        Calculate running sums of nums, if there is a duplicate entry at index i and j, then the
-        subarray with a summing to a multiple of k will be nums[i+1:j+1]
+        Calculate running sums of nums modulo k, if there is a duplicate running sum at
+        index i and j, then the subarray with a summing to a multiple of k will be
+        nums[i+1:j+1].
         O(n) / O(n)     time / space complexity
         """
         # set first seen of running sum 0 to 0, since if the running sum ever comes to zero after
@@ -31,15 +36,20 @@ class Solution:
         for i, num in enumerate(nums):
             running_sum = (num + running_sum) % k
 
-            # if current running sum has never been seen before, update first seen to NEXT index,
-            # to avoid wrong truthy return value in case next value is a multiple of k
             if running_sum not in first_seen:
+                # if current running sum has never been seen before, update first seen to
+                # NEXT index, to avoid wrong truthy return value in case next value is a
+                # multiple of k
                 first_seen[running_sum] = i + 1
 
-            # else if the running sum has been encountered, and it was not at the previous index
-            # (can only happen if num%k == 0) return True
             elif first_seen[running_sum] < i:
+                # else if the running sum has been encountered, and it was not at the
+                # previous index (can only happen if num%k == 0) return True
                 return True
 
         # no duplicate running sum found, return false
         return False
+
+
+s = Solution()
+s.checkSubarraySum([23, 2, 4, 6, 7], 6)
