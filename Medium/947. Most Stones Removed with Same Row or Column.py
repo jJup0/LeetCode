a75d5a4b3534e6739db719cd535b741b094ade08
Jakob Roithinger <jake.roithinger@gmail.com>
2024-08-29 18:17:52 +0200

    [947.] Updated

--- Medium/947. Most Stones Removed with Same Row or Column.py
+++ Medium/947. Most Stones Removed with Same Row or Column.py
@@ -1,25 +1,23 @@
-from collections import defaultdict
-from typing import List
+"""
+On a 2D plane, we place n stones at some integer coordinate points. Each
+coordinate point may have at most one stone.
+
+A stone can be removed if it shares either the same row or the same column as
+another stone that has not been removed.
+
+Given an array stones of length n where stones[i] = [x_i, y_i] represents the
+location of the ith stone, return the largest possible number of stones that
+can be removed.
+
+Constraints:
+- 1 <= stones.length <= 1000
+- 0 <= x_i, y_i <= 10^4
+- No two stones are at the same coordinate point.
+"""
 
 
 class Solution:
-    """
-    On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may
-    have at most one stone.
-
-    A stone can be removed if it shares either the same row or the same column as another stone
-    that has not been removed.
-
-    Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith
-    stone, return the largest possible number of stones that can be removed.
-
-    Constraints:
-        1 <= stones.length <= 1000
-        0 <= xi, yi <= 10^4
-        No two stones are at the same coordinate point.
-    """
-
-    def removeStones(self, stones: List[List[int]]) -> int:
+    def removeStones(self, stones: list[list[int]]) -> int:
         """
         Basically a graph problem, stones are nodes, and there exists an edge between to stones, if
         they either share a x- or y-coordinate. Find find all trees in the graph, in each tree
@@ -28,7 +26,7 @@ class Solution:
         """
 
         # union find data structure
-        UF = {}
+        UF: dict[int, int] = {}
 
         # unionfind find method, updates parents along the way O(1) amortized time complexity
         def find(x: int) -> int:
@@ -46,7 +44,7 @@ class Solution:
         # x and y are guaranteed non negative, so just negate y and subtract one, to make 0
         # unambiguous (-y-1 == ~y)
         for x, y in stones:
-            union(x, -y-1)
+            union(x, -y - 1)
 
         # find total number of roots (trees) in the union set, these cannot be removed, so subtract
         # that number from the total stone count for the result
