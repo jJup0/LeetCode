fe0238c0cb335e1186d6d76b7e3ff3a58509214a
Jakob Roithinger <jake.roithinger@gmail.com>
2025-03-31 18:02:50 +0200

    [763.] Updated

--- Medium/763. Partition Labels.py
+++ Medium/763. Partition Labels.py
@@ -1,54 +1,24 @@
-from typing import List
-
-
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        ord_a = ord('a')
+    def partitionLabels(self, s: str) -> list[int]:
+        """
+        Create intervals of first and last occurance of characters
+        and merge overlapping intervals.
 
-        # store first and last occurances of letters
-        # using lists instead of dicts makes zipping easier, also they have fixed size of 26 due to:
-        # constraint: s consists of lowercase English letters.
-        starts = [-1] * 26
-        ends = [-1] * 26
-
-        # find first and last occurances of letters
-        for i, c in enumerate(s):
-            # index in the starts/ends list is ord(c) - ord('a')
-            ord_c_standard = ord(c) - ord_a
-            if starts[ord_c_standard] == -1:
-                starts[ord_c_standard] = ends[ord_c_standard] = i
-            else:
-                ends[ord_c_standard] = i
-
-        # interval lenght lists
-        res = []
-
-        # start and end of "ongoing" interval
-        prev_start = prev_end = -1
-
-        # sort intervals, to find overlaps easily
-        for start, end in sorted(interval for interval in zip(starts, ends)):
-            # if start == -1, the letter was not present so there is no interval
-            if start == -1:
+        Complexity:
+            x := unique characters in string length of alphabet
+            Time: O(n * x) (O(n) is easily possible, but inputs are so small it does not matter)
+            Space: O(x)
+        """
+        intervals = sorted((s.find(char), s.rfind(char)) for char in set(s))
+        prev_start, prev_end = intervals[0]
+        res: list[int] = []
+        for start, stop in intervals:
+            if stop < prev_end:
                 continue
-
-            # intervals overlap exactly when the start is before the current end of the
-            # ongoing interval, to to the intervals being sorted
-            if start < prev_end:
-                # extend the ongoing interval end index if necessary
-                prev_end = max(prev_end, end)
-            else:
-                # if ongoing interval is not the pseudo one defined at the start, add its
-                # length to the result list
-                if prev_end != -1:
-                    res.append(prev_end - prev_start + 1)
-
-                # reset ongoing interval start and end
+            if start > prev_end:
+                res.append(prev_end - prev_start + 1)
                 prev_start = start
-                prev_end = end
+            prev_end = stop
 
-        # add any interval that was still ongoing
-        # due to constraint: 1 <= s.length, this will never be the pseudo interval (-1,-1)
         res.append(prev_end - prev_start + 1)
-
         return res
