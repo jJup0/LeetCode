a57279c4afc6ae8ca7cb5495a42de90f08ec1a31
Jakob Roithinger <jakob.roithinger@icloud.com>
2025-05-25 09:25:33 +0100

    create bootstrap template for contests

---
+++ contest/contest_template.py
@@ -0,0 +1,31 @@
+import bisect
+import heapq
+import itertools
+import json
+import math
+import random
+import time
+from collections import Counter, defaultdict, deque
+from functools import cache
+from typing import Any, Dict, List, Optional
+
+from sortedcontainers import SortedList
+
+MOD = 10**9 + 7
+
+
+class Solution:
+    pass
+
+
+def test():
+
+    s = Solution()
+    res = s.___________(
+        1,
+    )
+    real = True
+    assert res == real, res
+
+
+test()
