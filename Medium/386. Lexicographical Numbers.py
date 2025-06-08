ab2ac062e59c0a28c6b99b0615984c96ef2fc399
Jakob Roithinger <jakob.roithinger@icloud.com>
2025-06-08 07:49:42 +0100

    [386.] Updated

--- Medium/386. Lexicographical Numbers.py
+++ Medium/386. Lexicographical Numbers.py
@@ -14,7 +14,13 @@ import heapq
 class Solution:
     def lexicalOrder(self, n: int) -> list[int]:
         """
-        O(n) / O(1)     time / space complexity
+        Keep track of current number. Add trailing zeros if this keeps the
+        current number below n. Otherwise add 1 and remove all trailing zeros.
+
+        Removal of trailing zeroes accounts for 10% extra steps.
+        Complexity:
+            Time: O(n)
+            Space: O(1)
         """
         res: list[int] = []
         curr = 1
@@ -24,16 +30,23 @@ class Solution:
                 # adding extra 0 gives next lexicographical number
                 curr *= 10
             else:
-                # if number ends in 9, or previous number was n, divide by 10 to get next lexicographical
-                while curr % 10 == 9 or curr >= n:
-                    curr //= 10
+                # if we reached n we have to remove trailing digit
+                if curr == n:
+                    curr = curr // 10
+                # next lexicographical number is next number
                 curr += 1
+                # but we remove next number's trailing zeros, these will be
+                # added back in subsequent numbers
+                while curr % 10 == 0:
+                    curr //= 10
         return res
 
     def lexicalOrder_heap(self, n: int) -> list[int]:
         """
-        Technically against the "rules", but I count log(n) as constant, in this case it is 4.
-        O(n * log(n)) / O(log(n))     time / space complexity
+        Technically against the "rules", but here log(n) == 4 which imo is constant.
+        Complexity:
+            Time: O(n * log(n))
+            Space: O(log(n))
         """
         # create heap of numbers to add
         heap: list[str] = []
@@ -54,6 +67,18 @@ class Solution:
                 heapq.heappush(heap, str(num_int + 1))
         return res
 
-    def _lexical_order_trivial(self, n: int) -> list[int]:
+    def lexicalOrder_trivial(self, n: int) -> list[int]:
+        """Trivial implementation, used to check faster implementations."""
         strs = sorted(str(i) for i in range(1, n + 1))
         return [int(s) for s in strs]
+
+
+def test():
+    sol = Solution()
+    for i in range(1000):
+        res = sol.lexicalOrder(i)
+        real = sol.lexicalOrder_trivial(i)
+        assert res == real
+
+
+test()
