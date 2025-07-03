69570b1c3ab86c46a2c54c336acf14924533cad6
Jakob Roithinger <jakob.roithinger@icloud.com>
2025-07-03 18:01:39 +0100

    [3304.] Solved

---
+++ Easy/3304. Find the K-th Character in String Game I.py
@@ -0,0 +1,29 @@
+"""
+Alice and Bob are playing a game. Initially, Alice has a string word ="a".
+
+You are given a positive integer k.
+
+Now Bob will ask Alice to perform the following operation forever:
+- Generate a new string by changing each character in word to its next
+  character in the English alphabet, and append it to the original word.
+
+For example, performing the operation on"c" generates"cd" and performing the
+operation on"zb" generates"zbac".
+
+Return the value of the kth character in word, after enough operations have
+been done for word to have at least k characters.
+
+Note that the character'z' can be changed to'a' in the operation.
+
+Constraints:
+- 1 <= k <= 500
+"""
+
+
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        arr = ["a"]
+        while len(arr) < k:
+            for i in range(len(arr)):
+                arr.append(chr(ord(arr[i]) + 1))
+        return arr[k - 1]
